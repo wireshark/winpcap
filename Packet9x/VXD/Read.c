38a5d9a00b7746b6cff7d69746cad36802759f37
Loris Degioanni <loris.degioanni@polito.it>
2002-05-10 08:41:00 +0000

    Modified the constant representing the frequency of the timer.

--- Packet9x/VXD/Read.c
+++ Packet9x/VXD/Read.c
@@ -572,8 +572,8 @@ Packet_tap (	IN NDIS_HANDLE ProtocolBindingContext,
 		if( fres>uiBytesTransferred )fres=uiBytesTransferred;
 			lCapTime+=pOpen->StartTime;
 			header=(struct bpf_hdr*)CurrBuff;
-			header->bh_tstamp.tv_usec=(long)((lCapTime%1193182)*1000000/1193182);
-			header->bh_tstamp.tv_sec=(long)((lCapTime)/1193182);
+			header->bh_tstamp.tv_usec=(long)((lCapTime%1193046)*1000000/1193046);
+			header->bh_tstamp.tv_sec=(long)((lCapTime)/1193046);
 			header->bh_caplen=(UINT)fres;
 			header->bh_datalen=(UINT)uiPacketSize+uiHeaderBufferSize;
 			header->bh_hdrlen=sizeof(struct bpf_hdr);
