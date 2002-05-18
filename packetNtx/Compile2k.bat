7765be15d037a58f2f48d5316131b8665d02f53f
Gianluca Varenni <gianluca.varenni@polito.it>
2002-05-18 10:25:50 +0000

    Modifications to support the new TME extensions.

--- packetNtx/Compile2k.bat
+++ packetNtx/Compile2k.bat
@@ -1,6 +1,8 @@
 set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
 set NDISVERSION=NDIS50
+set TS_TYPE=RDTSC_TS
+if "%1"=="KQPC" set TS_TYPE=KQPC_TS
+if "%1"=="kqpc" set TS_TYPE=KQPC_TS
 md \i386\free
 md \i386\checked
-copy ..\common\win_bpf_filter.c driver
 build -c -w
