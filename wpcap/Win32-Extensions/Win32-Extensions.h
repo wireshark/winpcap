5a31c2bb2dff8646513252f872cfb01114ea8eca
Gianluca Varenni <gianluca.varenni@polito.it>
2002-05-31 18:47:33 +0000

    Added BPF opcodes for the TME extensions.

--- wpcap/Win32-Extensions/Win32-Extensions.h
+++ wpcap/Win32-Extensions/Win32-Extensions.h
@@ -36,3 +36,26 @@ u_int pcap_sendqueue_transmit(pcap_t *p, pcap_send_queue* queue, int sync);
 HANDLE pcap_getevent(pcap_t *p);
 int pcap_read_ex(pcap_t *p, struct pcap_pkthdr **pkt_header, u_char **pkt_data);
 int pcap_setuserbuffer(pcap_t *p, int size);
+
+#define		BPF_MEM_EX_IMM	0xc0
+#define		BPF_MEM_EX_IND	0xe0
+/*used for ST*/
+#define		BPF_MEM_EX		0xc0
+#define		BPF_TME					0x08
+
+#define		BPF_LOOKUP				0x90   
+#define		BPF_EXECUTE				0xa0
+#define		BPF_INIT				0xb0
+#define		BPF_VALIDATE			0xc0
+#define		BPF_SET_ACTIVE			0xd0
+#define		BPF_RESET				0xe0
+#define		BPF_SET_MEMORY			0x80
+#define		BPF_GET_REGISTER_VALUE	0x70
+#define		BPF_SET_REGISTER_VALUE	0x60
+#define		BPF_SET_WORKING			0x50
+#define		BPF_SET_ACTIVE_READ		0x40
+#define		BPF_SET_AUTODELETION	0x30
+#define		BPF_SEPARATION			0xff
+
+
+
