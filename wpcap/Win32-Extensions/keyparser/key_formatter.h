2e066221c43c41fb054661359dc52ae8f8ad36d2
Gianluca Varenni <gianluca.varenni@polito.it>
2002-07-11 06:41:30 +0000

    Moved the keyparser project under the win32-extensions of winpcap

---
+++ wpcap/Win32-Extensions/keyparser/key_formatter.h
@@ -0,0 +1,12 @@
+#ifndef __key_formatter__flex
+#define __key_formatter__flex
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int key_formatter(char *format, char *key, int key_len, char *output, int output_len);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
