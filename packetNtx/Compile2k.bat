7e5bc0787b8eac633b301ef8b12b72614ab89a52
Gianluca Varenni <gianluca.varenni@polito.it>
2002-07-15 06:47:28 +0000

    Default compilation uses KeQueryPerformanceCounter for timestamps. Use "compileXX rdstc" for RDTSC timestamps.

--- packetNtx/Compile2k.bat
+++ packetNtx/Compile2k.bat
@@ -1,8 +1,8 @@
 set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
 set NDISVERSION=NDIS50
-set TS_TYPE=RDTSC_TS
-if "%1"=="KQPC" set TS_TYPE=KQPC_TS
-if "%1"=="kqpc" set TS_TYPE=KQPC_TS
+set TS_TYPE=KQPC_TS
+if "%1"=="RDTSC" set TS_TYPE=RDTSC_TS
+if "%1"=="rdtsc" set TS_TYPE=RDTSC_TS
 md \i386\free
 md \i386\checked
 build -c -w
