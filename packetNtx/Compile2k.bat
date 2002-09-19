18d7cf7698003328cd298840ed498473adb88651
Gianluca Varenni <gianluca.varenni@polito.it>
2002-09-19 06:18:23 +0000

    Script cleanup.

--- packetNtx/Compile2k.bat
+++ packetNtx/Compile2k.bat
@@ -1,8 +1,8 @@
-set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
+@echo off
 set NDISVERSION=NDIS50
 set TS_TYPE=KQPC_TS
 if "%1"=="RDTSC" set TS_TYPE=RDTSC_TS
 if "%1"=="rdtsc" set TS_TYPE=RDTSC_TS
-md \i386\free
-md \i386\checked
-build -c -w
+md \i386\free >nul 2>nul
+md \i386\checked >nul 2>nul
+build -cefw
--- packetNtx/CompileNT4.bat
+++ packetNtx/CompileNT4.bat
@@ -1,8 +1,12 @@
-set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
+@echo off
+if NOT "%NPF_COMPILED%" == "1" (
+		set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
+		set NPF_COMPILED=1
+			)
 set NDISVERSION=NDIS30
 set TS_TYPE=KQPC_TS
 if "%1"=="RDTSC" set TS_TYPE=RDTSC_TS
 if "%1"=="rdtsc" set TS_TYPE=RDTSC_TS
-md \i386\free
-md \i386\checked
-build -c -w
+md \i386\free >nul 2>nul
+md \i386\checked >nul 2>nul
+build -cefw
