9f2d50d4f56c24298dfafc42a9f48f31cae022d4
Fulvio Risso <fulvio.risso@polito.it>
2003-02-14 09:52:59 +0000

    Temp file for docs generation; check with Loris

---
+++ gendoc.bat
@@ -0,0 +1,6 @@
+@echo Generate private documentation
+"d:\program files\doxygen\bin\doxygen" private.dox
+@echo Generate public documentation
+rem "d:\program files\doxygen\bin\doxygen" public.dox
+
+
