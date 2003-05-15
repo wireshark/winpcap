7ce257b405d99c9034da7f196a97ad57548d6d14
Loris Degioanni <loris.degioanni@polito.it>
2003-05-15 11:30:40 +0000

    Fixed PacketSendPackets() to respect the timestamp only if Sync is true.

--- packetNtx/Dll/Packet32.c
+++ packetNtx/Dll/Packet32.c
@@ -849,7 +849,6 @@ INT PacketSendPackets(LPADAPTER AdapterObject, PVOID PacketBuff, ULONG Size, BOO
 	struct timeval	BufStartTime;
 	LARGE_INTEGER	StartTicks, CurTicks, TargetTicks, TimeFreq;
 
-
 	ODS("PacketSendPackets");
 
 	// Obtain starting timestamp of the buffer
@@ -875,21 +874,24 @@ INT PacketSendPackets(LPADAPTER AdapterObject, PVOID PacketBuff, ULONG Size, BOO
 
 		TotBytesTransfered += BytesTransfered;
 
-		// calculate the time interval to wait before sending the next packet
-		TargetTicks.QuadPart = StartTicks.QuadPart +
-		(LONGLONG)
-		((((struct timeval*)((PCHAR)PacketBuff + TotBytesTransfered))->tv_sec - BufStartTime.tv_sec) * 1000000 +
-		(((struct timeval*)((PCHAR)PacketBuff + TotBytesTransfered))->tv_usec - BufStartTime.tv_usec)) *
-		(TimeFreq.QuadPart) / 1000000;
-
 		// Exit from the loop on termination or error
 		if(TotBytesTransfered >= Size || Res != TRUE)
 			break;
 		
-		// Wait until the time interval has elapsed
-		while( CurTicks.QuadPart <= TargetTicks.QuadPart )
-			QueryPerformanceCounter(&CurTicks);
+		if(Sync){
 
+			// calculate the time interval to wait before sending the next packet
+			TargetTicks.QuadPart = StartTicks.QuadPart +
+				(LONGLONG)
+				((((struct timeval*)((PCHAR)PacketBuff + TotBytesTransfered))->tv_sec - BufStartTime.tv_sec) * 1000000 +
+				(((struct timeval*)((PCHAR)PacketBuff + TotBytesTransfered))->tv_usec - BufStartTime.tv_usec)) *
+				(TimeFreq.QuadPart) / 1000000;
+
+			// Wait until the time interval has elapsed
+			while( CurTicks.QuadPart <= TargetTicks.QuadPart ){
+				QueryPerformanceCounter(&CurTicks);
+			}
+		}
 	}
 	while(TRUE);
 
