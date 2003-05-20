563f73a1bfc2c11c080be1728b5eebd2891ff7ab
Gianluca Varenni <gianluca.varenni@polito.it>
2003-05-20 10:03:56 +0000

    Added an option to make the deletion of files quiet.

--- createdoc.bat
+++ createdoc.bat
@@ -1,7 +1,7 @@
 @echo off
 cd dox\prj\
 @echo #### Delete existing WinPcap documentation
-del docs\*.*
+del /q docs\*.*
 @echo #### Create new documentation
 doxygen winpcap_c.dox
 @echo #### Copy 'winpcap_small.gif' file, since it is not copied
