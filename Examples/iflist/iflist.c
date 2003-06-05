5868b8901b291db49c7c0f5d5ce282b2c464670f
Fulvio Risso <fulvio.risso@polito.it>
2003-06-05 14:26:16 +0000

    Updated to findalldevs_ex

--- Examples/iflist/iflist.c
+++ Examples/iflist/iflist.c
@@ -1,43 +1,71 @@
 /*
- * Copyright (c) 1999 - 2002
- *	Politecnico di Torino.  All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that: (1) source code distributions
- * retain the above copyright notice and this paragraph in its entirety, (2)
- * distributions including binary code include the above copyright notice and
- * this paragraph in its entirety in the documentation or other materials
- * provided with the distribution, and (3) all advertising materials mentioning
- * features or use of this software display the following acknowledgement:
- * ``This product includes software developed by the Politecnico
- * di Torino, and its contributors.'' Neither the name of
- * the University nor the names of its contributors may be used to endorse
- * or promote products derived from this software without specific prior
- * written permission.
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR IMPLIED
- * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * Copyright (c) 1999 - 2003
+ * NetGroup, Politecnico di Torino (Italy)
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions 
+ * are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright 
+ * notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright 
+ * notice, this list of conditions and the following disclaimer in the 
+ * documentation and/or other materials provided with the distribution. 
+ * 3. Neither the name of the Politecnico di Torino nor the names of its 
+ * contributors may be used to endorse or promote products derived from 
+ * this software without specific prior written permission. 
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT 
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR 
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT 
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT 
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY 
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE 
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
  */
 
+
 #include "pcap.h"
+
 #ifndef WIN32
-#include <sys/socket.h>
-#include <netinet/in.h>
+	#include <sys/socket.h>
+	#include <netinet/in.h>
 #else
-#include <winsock.h>
+	#include <winsock.h>
 #endif
 
+
+// Function prototypes
 void ifprint(pcap_if_t *d);
 char *iptos(u_long in);
+
+
 
 int main()
 {
   pcap_if_t *alldevs;
   pcap_if_t *d;
   char errbuf[PCAP_ERRBUF_SIZE+1];
+  char source[PCAP_ERRBUF_SIZE+1];
+
+  printf("Enter the device you want to list:\n"
+			"rpcap://              ==> lists interfaces in the local machine\n"
+			"rpcap://hostname:port ==> lists interfaces in a remote machine\n"
+			"                          (rpcapd daemon must be up and running\n"
+			"                           and it must accept 'null' authentication)\n"
+			"file://foldername     ==> lists all pcap files in the give folder\n\n"
+			"Enter your choice: ");
+
+  gets(source);
 
   /* Retrieve the interfaces list */
-  if (pcap_findalldevs(&alldevs, errbuf) == -1)
+  if (pcap_findalldevs_ex(source, NULL, &alldevs, errbuf) == -1)
   {
     fprintf(stderr,"Error in pcap_findalldevs: %s\n",errbuf);
     exit(1);
@@ -51,6 +79,8 @@ int main()
 
   return 1;
 }
+
+
 
 /* Print all the available information on the given interface */
 void ifprint(pcap_if_t *d)
@@ -91,6 +121,8 @@ void ifprint(pcap_if_t *d)
   }
   printf("\n");
 }
+
+
 
 /* From tcptraceroute, convert a numeric IP address to a string */
 #define IPTOSBUFFERS	12
