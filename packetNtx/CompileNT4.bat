e3ef9da4315befae1f2746a4b03fef01983fc000
Gianluca Varenni <gianluca.varenni@polito.it>
2004-01-24 15:53:34 +0000

    Removed some useless stuff related to timestamping (since timestamping mode can be changed at runtime through a registry hive)

--- packetNtx/CompileNT4.bat
+++ packetNtx/CompileNT4.bat
@@ -4,9 +4,6 @@ if NOT "%NPF_COMPILED%" == "1" (
 		set NPF_COMPILED=1
 			)
 set NDISVERSION=NDIS30
-set TS_TYPE=KQPC_TS
-if "%1"=="RDTSC" set TS_TYPE=RDTSC_TS
-if "%1"=="rdtsc" set TS_TYPE=RDTSC_TS
 md \i386\free >nul 2>nul
 md \i386\checked >nul 2>nul
 build -cefw
