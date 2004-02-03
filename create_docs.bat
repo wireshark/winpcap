83ab665f6c8d04748e3b8d95796b18fd9abc7545
Gianluca Varenni <gianluca.varenni@polito.it>
2004-02-03 08:06:29 +0000

    Added a line to copy a missing picture in the developers pack

--- create_docs.bat
+++ create_docs.bat
@@ -17,8 +17,8 @@ call createdoc.bat
 popd
 
 xcopy /v /Y %WINPCAPSOURCEDIR%\dox\WinPcap_docs.html	%WPDPACKDESTDIR%\docs\
-xcopy /v /Y %WINPCAPSOURCEDIR%\dox\prj\docs\*.*			%WPDPACKDESTDIR%\docs\html\
-
+xcopy /v /Y %WINPCAPSOURCEDIR%\dox\prj\docs\*.*		%WPDPACKDESTDIR%\docs\html\
+xcopy /v /Y %WINPCAPSOURCEDIR%\dox\internals-arch.gif	%WPDPACKDESTDIR%\docs\html\
 echo Folder \docs created successfully
 set WPDPACKDESTDIR=
 set WINPCAPSOURCEDIR=
