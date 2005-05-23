c6ca5a42c9836ffdf91eb7472f440795fc08155c
Loris Degioanni <loris.degioanni@polito.it>
2005-05-23 19:54:25 +0000

    Updated the script to use the doxygen project without the C files

--- createdoc.bat
+++ createdoc.bat
@@ -3,7 +3,7 @@ cd dox\prj\
 @echo #### Delete existing WinPcap documentation
 del /q docs\*.*
 @echo #### Create new documentation
-doxygen winpcap_c.dox
+doxygen winpcap_noc.dox
 @echo #### Copy all gif files
 copy ..\pics\*.gif docs\. /v /y /q
 copy ..\*.gif docs\. /v /y /q
