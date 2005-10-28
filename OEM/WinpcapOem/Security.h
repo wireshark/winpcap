1baa099c343d4b21509bd5b76088bf45ba3bff4a
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-10-28 17:18:44 +0000

    Added the usual #ifdef/#define at the beginning of the include file. Fixed the prototype and name of IsProcAuthorized. Fixed the prototype and name of setProcAuthorization (to WoemGerCurrentProcessAuthorization). Now it doesn't modify a global variable, but instead it returns an error code with the possible statuses: - authorization ok - authorization denied - failure in computing the authorization Added a comment for this function. Removed some function definitions (moved to WinpcapOem.h).

--- OEM/WinpcapOem/Security.h
+++ OEM/WinpcapOem/Security.h
@@ -1,7 +1,28 @@
+#ifndef __SECURITY_H_820D0C9F_A563_489b_834A_23FBB6C892F7
+#define __SECURITY_H_820D0C9F_A563_489b_834A_23FBB6C892F7
+
 #include <windows.h>
 
-extern BOOL isProcAuthorized;
+enum
+{
+	WOEM_PROC_AUTHORIZATION_OK,
+	WOEM_PROC_AUTHORIZATION_DENIED,
+	WOEM_PROC_AUTHORIZATION_FAILURE,
+};
 
-INT setProcAuthorization();
+/*!
+	\brief verify if the current process is authorized to use WOEM
 
-BOOL WoemGetResource(HINSTANCE hInst, int ResID, LPVOID *lpResMem, LPDWORD dwResSize);
+	\param errString A caller allocated string (with a minimum size of
+	\ref PACKET_ERRSTR_SIZE bytes), that will return a meaningful error
+	message in case the function returns WOEM_PROC_AUTHORIZATION_FAILURE.
+
+	\return Possible return values are
+	- \ref WOEM_PROC_AUTHORIZATION_FAILURE An error occurred verifying the 
+	authoerization. A more detailed error is detailed in errString
+	- \ref WOEM_PROC_AUTHORIZATION_OK	The current process is authorized
+	- \ref WOEM_PROC_AUTHORIZATION_DENIED The current process is not authorized
+*/
+INT WoemGetCurrentProcessAuthorization(char *errString);
+
+#endif //#ifndef __SECURITY_H_820D0C9F_A563_489b_834A_23FBB6C892F7
