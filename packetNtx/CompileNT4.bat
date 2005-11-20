884e314fbccc7ef0cb71514bda5576221447374d
Gianluca Varenni <gianlucav@winpcap.org>
2005-11-20 18:56:33 +0000

    Cleaned up the script generating the x86 NT4 driver: - added a check that generates an error if the build environment is not the     correct one. - added some code to copy Sources.x86 to Sources before the build, and     delete Sources after the build. - added some code to set the environment variable NPF_ARCH to     __NPF_x86__.

--- packetNtx/CompileNT4.bat
+++ packetNtx/CompileNT4.bat
@@ -3,7 +3,25 @@ if NOT "%NPF_COMPILED%" == "1" (
 		set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
 		set NPF_COMPILED=1
 			)
+
+if NOT %_BUILDARCH%==x86 goto error_buildenv
+
+set NPF_ARCH=__NPF_x86__
 set NDISVERSION=NDIS30
 md \i386\free >nul 2>nul
 md \i386\checked >nul 2>nul
+
+echo foo > driver\Sources
+xcopy driver\Sources.x86 driver\Sources /y /v /q 
+
 build -cefw
+
+del driver\Sources
+
+goto end
+
+:error_buildenv
+
+echo Wrong build Environment. Please use the Build Environment for x86 (32 bit)
+
+:end
