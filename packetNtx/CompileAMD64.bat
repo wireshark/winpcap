c1e7e9e4195c5acec1c2550c50ec2843226324f0
Gianluca Varenni <gianlucav@winpcap.org>
2005-11-20 18:57:35 +0000

    Added a script to compile the NPF driver on AMD64.

---
+++ packetNtx/CompileAMD64.bat
@@ -0,0 +1,22 @@
+@echo off
+if NOT %_BUILDARCH%==AMD64 goto error_buildenv
+
+set NDISVERSION=NDIS50
+set NPF_ARCH=__NPF_AMD64__
+md \i386\free >nul 2>nul
+md \i386\checked >nul 2>nul
+
+echo foo > driver\Sources
+xcopy driver\Sources.AMD64 driver\Sources /y /v /q 
+
+build -cefw
+
+del /f /q driver\Sources 
+
+goto end
+
+:error_buildenv
+
+echo Wrong build Environment. Please use the Build Environment for x86-64 (AMD64)
+
+:end
