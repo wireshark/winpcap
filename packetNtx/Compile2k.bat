9317ffaacfb89722c401d9a2bb0ffa246b8ae575
Gianluca Varenni <gianlucav@winpcap.org>
2005-11-20 18:55:56 +0000

    Cleaned up the script generating the x86 NT2k/XP/2003/... driver: - added a check that generates an error if the build environment is not the   correct one. - added some code to copy Sources.x86 to Sources before the build, and   delete Sources after the build. - added some code to set the environment variable NPF_ARCH to   __NPF_x86__.

--- packetNtx/Compile2k.bat
+++ packetNtx/Compile2k.bat
@@ -1,5 +1,22 @@
 @echo off
+if NOT %_BUILDARCH%==x86 goto error_buildenv
+
 set NDISVERSION=NDIS50
+set NPF_ARCH=__NPF_x86__
 md \i386\free >nul 2>nul
 md \i386\checked >nul 2>nul
+
+echo foo > driver\Sources
+xcopy driver\Sources.x86 driver\Sources /y /v /q 
+
 build -cefw
+
+del driver\Sources
+
+goto end
+
+:error_buildenv
+
+echo Wrong build Environment. Please use the Build Environment for x86 (32 bit)
+
+:end
