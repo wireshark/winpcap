e9d358e64cbb53be3300c91429d9ee14c72b4621
Gianluca Varenni <gianlucav@winpcap.org>
2005-11-20 18:43:39 +0000

    Updated the copyright to the new CACE+NetGroup one. Added some #ifdef's to free the JIT filter code in the x86-32 bit driver only.

--- packetNtx/driver/Openclos.c
+++ packetNtx/driver/Openclos.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 1999 - 2003
- * NetGroup, Politecnico di Torino (Italy)
+ * Copyright (c) 1999 - 2005 NetGroup, Politecnico di Torino (Italy)
+ * Copyright (c) 2005 CACE Technologies, Davis (California)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,9 +12,10 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Politecnico di Torino nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
+ * 3. Neither the name of the Politecnico di Torino, CACE Technologies 
+ * nor the names of its contributors may be used to endorse or promote 
+ * products derived from this software without specific prior written 
+ * permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -413,9 +414,14 @@ NPF_Close(IN PDEVICE_OBJECT DeviceObject,IN PIRP Irp)
 		if(Open->bpfprogram != NULL)
 			ExFreePool(Open->bpfprogram);
 
+//
+// Jitted filters are supported on x86 (32bit) only
+// 
+#ifdef __NPF_x86__
 		// Free the jitted filter if it's present
 		if(Open->Filter != NULL)
 			BPF_Destroy_JIT_Filter(Open->Filter);
+#endif
 
 		//free the buffer
 //		Open->BufSize=0;
@@ -516,10 +522,15 @@ NPF_CloseAdapterComplete(IN NDIS_HANDLE  ProtocolBindingContext,IN NDIS_STATUS  
 		if(Open->bpfprogram != NULL)
 			ExFreePool(Open->bpfprogram);
 
+//
+// Jitted filters are supported on x86 (32bit) only
+// 
+#ifdef __NPF_x86__
 		// Free the jitted filter if it's present
 		if(Open->Filter != NULL)
 			BPF_Destroy_JIT_Filter(Open->Filter);
-		
+#endif // __NPF_x86__
+
 		//free the buffer
 //		Open->BufSize = 0;
 //		if(Open->Buffer!=NULL)ExFreePool(Open->Buffer);
