75d08c57ba5b6b943ce767b1f47eb127c93586c9
Gianluca Varenni <gianlucav@winpcap.org>
2005-11-24 19:53:01 +0000

    Cleaned up all the strings. Added some defines for the registry keys. Fixed a problem with the license (ano old reference to Politecnico di Torino).

--- Common/WpcapNames.h
+++ Common/WpcapNames.h
@@ -11,7 +11,7 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Politecnico di Torino nor the names of its
+ * 3. Neither the name of CACE Technologies nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
  *
@@ -35,44 +35,45 @@
 //
 // Original names
 //
-#define NPF_DRIVER_NAME							"NPF"
-#define NPF_DRIVER_NAME_WIDECHAR				L"NPF"
+#define NPF_DRIVER_NAME							"GVO"					///< (HHH) Packet.dll
+#define NPF_DRIVER_NAME_WIDECHAR				L"GVO"					///< (HHH) Packet.dll
 
 //
 // Derived strings
 //
-
-#define NPF_SERVICE_DESC_WIDECHAR				L"WinPcap Packet Driver"
-#define NPF_SERVICE_REGISTRY_LOCATION_WIDECHAR	L"SYSTEM\\CurrentControlSet\\Services\\" NPF_DRIVER_NAME_WIDECHAR
-#define NPF_COMPLETE_SERVICE_REGISTRY_LOCATION	"HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Services\\" NPF_DRIVER_NAME
-#define NPF_DRIVER_PATH_WIDECHAR				L"system32\\drivers\\"
-#define NPF_DRIVER_BINARY_WIDECHAR				NPF_DRIVER_NAME_WIDECHAR L".sys"
-
-//Not in use, now
-//#define NPF_DRIVER_COMPLETE_WIDECHAR			NPF_DRIVER_PATH_WIDECHAR NPF_DRIVER_BINARY_WIDECHAR
-
-//#define NPF_SERVICE_DESC_ASCII					"WinPcap Packet Driver (" NPF_DRIVER_NAME ")"
-#define NPF_DRIVER_PATH_ASCII					"system32\\drivers\\"
-#define NPF_DRIVER_BINARY_ASCII					NPF_DRIVER_NAME ".sys"
-
-#define NPF_DEVICE_NAMES_PREFIX NPF_DRIVER_NAME "_"
-#define NPF_DEVICE_NAMES_PREFIX_WIDECHAR NPF_DRIVER_NAME_WIDECHAR L"_"
-#define NPF_DRIVER_COMPLETE_DRIVER_PREFIX "\\Device\\" NPF_DRIVER_NAME "_"
-#define NPF_USER_EVENTS_NAMES NPF_DRIVER_NAME_WIDECHAR L"0000000000"
-#define NPF_KERNEL_EVENTS_NAMES L"\\BaseNamedObjects\\" NPF_USER_EVENTS_NAMES L"\0"
-
-#define FAKE_NDISWAN_ADAPTER_NAME "\\Device\\" NPF_DRIVER_NAME "_GenericDialupAdapter"  ///< Name of a fake ndiswan adapter that is always available on 2000/XP/2003, used to capture NCP/LCP packets
-#define FAKE_NDISWAN_ADAPTER_DESCRIPTION "Adapter for generic dialup and VPN capture"	///< Description of a fake ndiswan adapter that is always available on 2000/XP/2003, used to capture NCP/LCP packets
+#define NPF_DEVICE_NAMES_PREFIX				NPF_DRIVER_NAME "_"     								///< (AAA) packet.dll
+#define NPF_DEVICE_NAMES_PREFIX_WIDECHAR	NPF_DRIVER_NAME_WIDECHAR L"_"     						///< (AAA) used by the NPF driver, that does not accept the TEXT(a) macro correctly.
+#define NPF_EVENTS_NAMES					NPF_DRIVER_NAME											///< (BBB) 
+#define NPF_EVENTS_NAMES_WIDECHAR			NPF_DRIVER_NAME_WIDECHAR								///< (BBB) used by the NPF driver, that does not accept the TEXT(a) macro correctly.
+#define FAKE_NDISWAN_ADAPTER_NAME			"\\Device\\" NPF_DRIVER_NAME "_GenericDialupAdapter"	///< (CCC) Name of a fake ndiswan adapter that is always available on 2000/XP/2003, used to capture NCP/LCP packets
+#define FAKE_NDISWAN_ADAPTER_DESCRIPTION	"Adapter for generic dialup and VPN capture"			///< (DDD) Description of a fake ndiswan adapter that is always available on 2000/XP/2003, used to capture NCP/LCP packets
+#define NPF_SERVICE_DESC					"WinPcap Packet Driver (" NPF_DRIVER_NAME ")"			///< (FFF) packet.dll
+#define NPF_DRIVER_COMPLETE_DEVICE_PREFIX	"\\Device\\" NPF_DRIVER_NAME "_"						///< (III) packet.dll
+#define NPF_DRIVER_COMPLETE_PATH			"system32\\drivers\\" NPF_DRIVER_NAME ".sys"			///< (LLL) packet.dll
 
 
 //
 // WinPcap Global Registry Key
 //
-#define WINPCAP_GLOBAL_KEY "SOFTWARE\\CaceTech\\WinPcapOem"
-#define WINPCAP_GLOBAL_KEY_WIDECHAR L"SOFTWARE\\CaceWinPcap"
-#define WINPCAP_INSTANCE_KEY WINPCAP_GLOBAL_KEY "\\" NPF_DRIVER_NAME
-#define WINPCAP_INSTANCE_KEY_WIDECHAR WINPCAP_GLOBAL_KEY_WIDECHAR L"\\" NPF_DRIVER_NAME_WIDECHAR
+#define WINPCAP_GLOBAL_KEY				"SOFTWARE\\CaceTech\\WinPcapOem"
+#define WINPCAP_GLOBAL_KEY_WIDECHAR		L"SOFTWARE\\CaceTech\\WinPcapOem"
+#define WINPCAP_INSTANCE_KEY			WINPCAP_GLOBAL_KEY "\\" NPF_DRIVER_NAME
+#define WINPCAP_INSTANCE_KEY_WIDECHAR	WINPCAP_GLOBAL_KEY_WIDECHAR	L"\\" NPF_DRIVER_NAME_WIDECHAR
 #define MAX_WINPCAP_KEY_CHARS 512
+
+//
+// Subkeys names
+//
+#define NPF_DEVICES_PREFIX_REG_KEY				"NpfDeviceNamesPrefix"		///< (AAA) 
+#define NPF_DEVICES_PREFIX_REG_KEY_WC			L"NpfDeviceNamesPrefix"		///< (AAA) used by the NPF driver, that does not accept the TEXT(a) macro correctly.
+#define NPF_EVENTS_NAMES_REG_KEY				"NpfEventsNames"			///< (BBB) 
+#define NPF_EVENTS_NAMES_REG_KEY_WC				L"NpfEventsNames"			///< (BBB) used by the NPF driver, that does not accept the TEXT(a) macro correctly.	
+#define NPF_FAKE_NDISWAN_ADAPTER_NAME_REG_KEY	"NdiswanAdapterName"		///< (CCC) packet.dll
+#define NPF_FAKE_NDISWAN_ADAPTER_DESC_REG_KEY	"NdiswanAdapterDescription"	///< (DDD) packet.dll
+#define NPF_SERVICE_DESC_REG_KEY				"NpfServiceDescription"		///< (FFF) packet.dll
+#define NPF_DRIVER_NAME_REG_KEY					"NpfDriverName"				///< (HHH) packet.dll
+#define NPF_DRIVER_COMPLETE_DEVICE_PREFIX_REG_KEY "NpfCompleteDriverPrefix"	///< (III) packet.dll
+#define NPF_DRIVER_COMPLETE_PATH_REG_KEY		"NpfDriverCompletePath"		///< (LLL) 
 
 #endif //__WPCAPNAMES_H_EED6D131C6DB4dd696757D219977A7E5
 
