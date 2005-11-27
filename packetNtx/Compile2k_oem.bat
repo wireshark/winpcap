75c34abd5a52d90a8e0d07127504ad987f0ff057
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-11-27 02:14:49 +0000

    Cleaned up the build scripts for NT4 and 2k/XP/2003/vista: now we pass any additional define for the build with the environment variable USER_C_DEFINES.

--- packetNtx/Compile2k_oem.bat
+++ packetNtx/Compile2k_oem.bat
@@ -1,6 +1,11 @@
 @echo off
-set NDISVERSION=NDIS50
-set OEM=WPCAP_OEM
+
+set USER_C_DEFINES=-DNDIS50 -DWPCAP_OEM
+
 md \i386\free >nul 2>nul
 md \i386\checked >nul 2>nul
+
 build -cefw
+
+set USER_C_DEFINES=
+
--- packetNtx/CompileNT4_oem.bat
+++ packetNtx/CompileNT4_oem.bat
@@ -3,8 +3,8 @@ if NOT "%NPF_COMPILED%" == "1" (
 		set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
 		set NPF_COMPILED=1
 			)
-set NDISVERSION=NDIS30
-set OEM=WPCAP_OEM
+set USER_C_DEFINES=-D__NPF_NT4__ -DNDIS30 -DWPCAP_OEM
 md \i386\free >nul 2>nul
 md \i386\checked >nul 2>nul
 build -cefw
+set USER_C_DEFINES=
