462a77d41e417a50da635960747d0d12d21f40b9
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-11-28 20:55:15 +0000

    Cleaned up the script: - now it's possible to execute it directly from here, without moving the file - added some code to set the target directory for the driver.

--- packetNtx/CompileNT4_oem.bat
+++ packetNtx/CompileNT4_oem.bat
@@ -4,7 +4,21 @@ if NOT "%NPF_COMPILED%" == "1" (
 		set NPF_COMPILED=1
 			)
 set USER_C_DEFINES=-D__NPF_NT4__ -DNDIS30 -DWPCAP_OEM
-md \i386\free >nul 2>nul
-md \i386\checked >nul 2>nul
+set TARGETPATH=..\..\..\winpcap_oem\PacketNtx\driver\bin\NT4
+
+mkdir driver\bin 2> nul
+mkdir driver\bin\NT4 2> nul
+mkdir driver\bin\NT4\i386\ 2> nul
+mkdir driver\bin\NT4\i386\free 2> nul
+mkdir driver\bin\NT4\i386\checked 2> nul
+
+pushd ..\..\winpcap\packetntx\driver
+
 build -cefw
+
+popd
+
+copy driver\bin\NT4\i386\%DDKBUILDENV%\*.* driver\bin\NT4\i386\
+
+set TARGETPATH=
 set USER_C_DEFINES=
