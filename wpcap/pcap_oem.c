931cdb8fb2298e889bcfb01b93bcca4efaeee3de
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-11-30 22:09:25 +0000

    Added a new source file containing pcap_start_oem() (used by WinPcap OEM only).

---
+++ wpcap/pcap_oem.c
@@ -0,0 +1,26 @@
+#include <pcap-int.h>
+
+/*!
+	\name OEM-specific Functions
+*/
+//\{ 
+
+/*!
+	\ingroup wpcapfunc 
+	\brief This function starts WinPcap OEM.
+	
+	\param err_str pointer to a user-allocated buffer (of minimum size PCAP_ERRBUF_SIZE) 
+		that will contain the error message in case of failure.
+	\param flags Reserved. It should be set to 0.
+
+	\return 0 on success,  -1 in case of failure.
+*/
+int pcap_start_oem(char* err_str, int flags)
+{
+	if(PacketStartOem(err_str, PCAP_ERRBUF_SIZE) == FALSE)
+	{
+		return -1;
+	}
+	return 0;
+}
+//\}
