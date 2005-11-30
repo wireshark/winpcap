d8f2bde4b7a10b231111f9b2c56585818e0d25e6
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-11-30 17:22:19 +0000

    Added a global version file for the entire WinPcap OEM project.

---
+++ version.h
@@ -0,0 +1,30 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+// Used by NetMonInstaller.rc
+//
+
+// 0 Feb = 31 + 0 = 31
+// 0 Mar = 31 + 28 + 0 = 59
+// o Apr = 31 + 28 + 31 = 90
+// 0 May = 31 + 28 + 31 + 30 = 120
+// 0 Jun = 31 + 28 + 31 + 30 + 31 = 151 
+// 0 Jul = 31 + 28 + 31 + 30 + 31 + 30 = 181
+// 0 Nov = 181 + (Jul) 31 + (Aug) 31 + (Sept)30 + (Oct)31 = 325
+// 0 Dec = 325 + 30 = 335
+
+#define WINPCAPOEM_MAJOR	3
+#define WINPCAPOEM_MINOR	2
+#define WINPCAPOEM_REV		1
+#define WINPCAPOEM_BUILD	334
+
+#define WINPCAPOEM_VER_STRING	"3.2.1.334.tnt"
+
+#define WINPCAPOEM_COMPANY_NAME 			"CACE Technologies"
+
+#define WINPCAPOEM_PACKET_WRAPPER_NAME 		"Packet.dll Wrapper"
+#define WINPCAPOEM_VERSION_STRING 			"1"
+#define WINPCAPOEM_PRODUCT_NAME 			"WinPcap OEM"
+#define WINPCAPOEM_COPYRIGHT_STRING 		"Copyright © 2005 CACE Technologies"
+#define WINPCAPOEM_PACKET_WRAPPER_FILENAME 	"Packet.dll (wrapper)"
+#define WINPCAPOEM_BUILD_DESCRIPTION 		""
+#define WINPCAPOEM_PRIVATE_BUILD		"Test Versioning BUILD"
