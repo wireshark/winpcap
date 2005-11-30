7af139d800936a02a356ebee95be979ef0ce8c8c
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-11-30 17:25:29 +0000

    Added some code to copy the proper NPF.RC version file into the driver directory. Added some environment flags to generate PDB files in both the free and checked version of the driver.

--- packetNtx/CompileNT4_oem.bat
+++ packetNtx/CompileNT4_oem.bat
@@ -6,11 +6,17 @@ if NOT "%NPF_COMPILED%" == "1" (
 set USER_C_DEFINES=-D__NPF_NT4__ -DNDIS30 -DWPCAP_OEM
 set TARGETPATH=..\..\..\winpcap_oem\PacketNtx\driver\bin\NT4
 
+REM this enables the generation of pdb files for both free and debug mode
+rem set USE_PDB=1
+rem set NTDEBUGTYPE=windbg
+
 mkdir driver\bin 2> nul
 mkdir driver\bin\NT4 2> nul
 mkdir driver\bin\NT4\i386\ 2> nul
 mkdir driver\bin\NT4\i386\free 2> nul
 mkdir driver\bin\NT4\i386\checked 2> nul
+
+copy driver\NPF_NT4.RC ..\..\winpcap\packetntx\driver\NPF.RC
 
 pushd ..\..\winpcap\packetntx\driver
 
@@ -20,6 +26,7 @@ popd
 
 if "%DDKBUILDENV%" == "free" (
 		copy driver\bin\NT4\i386\free\npf.sys driver\bin\NT4\i386\
+		copy driver\bin\NT4\i386\free\npf.pdb driver\bin\NT4\i386\
 			)
 
 if "%DDKBUILDENV%" == "checked" (
@@ -28,3 +35,5 @@ if "%DDKBUILDENV%" == "checked" (
 			)
 set TARGETPATH=
 set USER_C_DEFINES=
+rem set USE_PDB=
+rem set NTDEBUGTYPE=
