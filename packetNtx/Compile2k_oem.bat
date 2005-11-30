24c56f40c9da073172ae931e2668a32de14ac014
Gianluca Varenni <gianluca.varenni@cacetech.com>
2005-11-30 17:24:43 +0000

    Added some code to copy the proper NPF.RC version file into the driver directory.

--- packetNtx/Compile2k_oem.bat
+++ packetNtx/Compile2k_oem.bat
@@ -6,6 +6,8 @@ mkdir driver\bin\2k 2> nul
 set USER_C_DEFINES=-DNDIS50 -DWPCAP_OEM
 set TARGETPATH=..\..\..\winpcap_oem\PacketNtx\driver\bin\2k
 
+copy driver\NPF_NT5.RC ..\..\winpcap\packetntx\driver\NPF.RC
+
 pushd ..\..\winpcap\packetntx\driver
 
 prefast build -cefw
