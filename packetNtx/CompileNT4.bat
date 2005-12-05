2b036c5a6bd856fcb2c597bcebc609e30900454d
Gianluca Varenni <gianlucav@winpcap.org>
2005-12-05 00:14:09 +0000

    Commented out the check for _BUILDARCH, since it's not defined in the NT4 DDK.

--- packetNtx/CompileNT4.bat
+++ packetNtx/CompileNT4.bat
@@ -4,7 +4,7 @@ if NOT "%NPF_COMPILED%" == "1" (
 		set NPF_COMPILED=1
 			)
 
-if NOT %_BUILDARCH%==x86 goto error_buildenv
+rem if NOT "%_BUILDARCH%"=="x86" goto error_buildenv
 
 set NPF_ARCH=__NPF_x86__
 set NDISVERSION=NDIS30
@@ -12,7 +12,7 @@ md \i386\free >nul 2>nul
 md \i386\checked >nul 2>nul
 
 echo foo > driver\Sources
-xcopy driver\Sources.x86 driver\Sources /y /v /q 
+xcopy driver\Sources.x86 driver\Sources /v /q 
 
 build -cefw
 
