a896ec2073fa625e0757c055f460dbd223fb1c34
Gianluca Varenni <gianlucav@winpcap.org>
2006-03-29 15:55:04 +0000

    Updated the copyright to the new CACE+NetGroup one. Substituted ExAllocatePool with ExAllocatePoolWithTag to avoid a PREfast warning about untagged memory allocations.

--- packetNtx/driver/memory_t.h
+++ packetNtx/driver/memory_t.h
@@ -1,6 +1,6 @@
 /*
- * Copyright (c) 2001 - 2003
- * NetGroup, Politecnico di Torino (Italy)
+ * Copyright (c) 2001 - 2005 NetGroup, Politecnico di Torino (Italy)
+ * Copyright (c) 2005 - 2006 CACE Technologies, Davis (California)
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -12,9 +12,10 @@
  * 2. Redistributions in binary form must reproduce the above copyright
  * notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 3. Neither the name of the Politecnico di Torino nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
+ * 3. Neither the name of the Politecnico di Torino, CACE Technologies 
+ * nor the names of its contributors may be used to endorse or promote 
+ * products derived from this software without specific prior written 
+ * permission.
  *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -100,10 +101,10 @@ __inline VOID SW_ULONG_ASSIGN(void *dst, uint32 src)
 #ifdef WIN_NT_DRIVER
 
 #define ALLOCATE_MEMORY(dest,type,amount) \
-	  (dest)=ExAllocatePool(NonPagedPool,sizeof(type)*(amount));
+	  (dest)=ExAllocatePoolWithTag(NonPagedPool,sizeof(type)*(amount), '0TWA');
 #define ALLOCATE_ZERO_MEMORY(dest,type,amount) \
 	{ \
-		(dest)=ExAllocatePool(NonPagedPool,sizeof(type)*(amount)); \
+		(dest)=ExAllocatePoolWithTag(NonPagedPool,sizeof(type)*(amount), '1TWA'); \
 		if ((dest)!=NULL) \
 			RtlZeroMemory((dest),sizeof(type)*(amount)); \
 	}	
