81dc05c1e12aa80a6d59387a991ea400a4955015
Gianluca Varenni <gianlucav@winpcap.org>
2006-05-08 17:33:12 +0000

    Cleaned up the script to copy the new Installer helper DLL.
    Added some ">nul" redirections to the mkdir commands.

--- install/copy_installer_files.bat
+++ install/copy_installer_files.bat
@@ -1,9 +1,9 @@
 @echo off
-mkdir installer\distribution
-mkdir installer\distribution\2000
-mkdir installer\distribution\NT
-mkdir installer\distribution\9x
-mkdir installer\distribution\x86-64
+mkdir installer\distribution		 2>nul >nul
+mkdir installer\distribution\2000	 2>nul >nul
+mkdir installer\distribution\NT		 2>nul >nul
+mkdir installer\distribution\9x		 2>nul >nul
+mkdir installer\distribution\x86-64	 2>nul >nul
 
 copy ..\packetNtx\driver\bin\2k\i386\npf.sys installer\distribution\2000\
 copy ..\packetNtx\driver\bin\2k\i386\npf.pdb installer\distribution\2000\
@@ -18,12 +18,17 @@ copy ..\packetNtx\Dll\Project\NT4_Release\Packet.dll installer\distribution\NT\
 copy ..\packetNtx\Dll\WanPacket\Release\WanPacket.dll installer\distribution\2000\
 echo REMEMBER TO COPY npf.vxd
 pause
+
 copy ..\packet9x\DLL\Project\Release\Packet.dll installer\distribution\9x\
 copy ..\wpcap\PRJ\Release_REMOTE\wpcap.dll installer\distribution\
 copy ..\wpcap\PRJ\Release_REMOTE\rpcapd.exe installer\distribution\2000\
-copy daemon_mgm\Release\daemon_mgm.exe installer\distribution\2000\
-copy NetMonInstaller\objfre_w2k_x86\i386\NetMonInstaller.exe installer\distribution\2000\
-copy npf_mgm\Release\npf_mgm.exe installer\distribution\2000\
+
+
+rem copy daemon_mgm\Release\daemon_mgm.exe installer\distribution\2000\
+rem copy NetMonInstaller\objfre_w2k_x86\i386\NetMonInstaller.exe installer\distribution\2000\
+rem copy npf_mgm\Release\npf_mgm.exe installer\distribution\2000\
+
+copy "WinPcap Installer Helper\Release\WinPcapInstall.dll" installer\distribution\2000\
 
 copy ..\wpcap\libpcap\rpcapd\win32-pthreads\pthreadVC.dll installer\distribution\
 rem this nasty hack is needed to fix some time zone issues with the CVS...
