4478e5a3a12ce2b53ef6a3c75d7a0d2a374bf540
Gianluca Varenni <gianluca.varenni@cacetech.com>
2006-07-10 21:52:59 +0000

    Obsoleted the old compilation scripts.

--- packetNtx/Compile2k_oem.bat
+++ packetNtx/Compile2k_oem.bat
@@ -1,5 +1,16 @@
 @echo off
 
+echo ************************************************************
+echo *
+echo * This compilation script has been OBSOLETED.
+echo *
+echo * Please use CompileDriver.bat to compile the driver
+echo * for all the supported platforms (NT4,2k,xp,2k3,vista,x64)
+echo *
+echo ************************************************************
+
+goto end
+
 mkdir driver\bin 2> nul
 mkdir driver\bin\2k 2> nul
 
@@ -17,3 +28,4 @@ popd
 set TARGETPATH=
 set USER_C_DEFINES=
 
+:end
--- packetNtx/CompileNT4_oem.bat
+++ packetNtx/CompileNT4_oem.bat
@@ -1,4 +1,15 @@
 @echo off
+
+echo ************************************************************
+echo *
+echo * This compilation script has been OBSOLETED.
+echo *
+echo * Please use CompileDriver.bat to compile the driver
+echo * for all the supported platforms (NT4,2k,xp,2k3,vista,x64)
+echo *
+echo ************************************************************
+
+goto end
 if NOT "%NPF_COMPILED%" == "1" (
 		set Include=%BASEDIR%\src\network\inc;%BASEDIR%\inc;%Include%
 		set NPF_COMPILED=1
@@ -37,3 +48,5 @@ set TARGETPATH=
 set USER_C_DEFINES=
 rem set USE_PDB=
 rem set NTDEBUGTYPE=
+
+:end
