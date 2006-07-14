9842607460eca351c8c7a7e6aa3d00599b7326a8
Gianluca Varenni <gianluca.varenni@cacetech.com>
2006-07-14 16:55:25 +0000

    Modified WoemSaveResourceToDisk to enable/disable the WOW64 FS redirector depending on a parameter of the function. Removed some leftover code and debugging stuff from the file.

--- OEM/WinpcapOem/Resources.cpp
+++ OEM/WinpcapOem/Resources.cpp
@@ -1,41 +1,70 @@
 #include <windows.h>
 #include <stdio.h>
-
-//
-// The following include files contain the drivers and dlls that are converted
-// during the custom build step.
-//
-//#include "Packet2k.h"
-//#include "Packetnt.h"
-//#include "Npf2k.h"
-//#include "NpfNt.h"
-
-char* ResourceNames[] =
-{
-	"packet2k.dll",
-	"driver2k.dll"
-};
+#include "WinpcapOem.h"
 
 ////////////////////////////////////////////////////////////////////
 // This function locates one of the binary resources embedded in the
 // dll and saves it to disk
 ////////////////////////////////////////////////////////////////////
-BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName)
+BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName, BOOL bDisableFsRedirector)
 {
 	char ResName[100];
-	sprintf(ResName, "#%d", ResID);
 	FILE* OutFile;
+	PVOID OldFsRedirectorValue;
+	HMODULE hKernel32Dll = NULL;
+	Wow64DisableWow64FsRedirectionHandler DisableFsRedirector;
+	Wow64RevertWow64FsRedirectionHandler RevertFsRedirector;
 
 #ifdef STATIC_LIB
 	hInst = NULL;
 #endif
 
+	if (bDisableFsRedirector)
+	{
+		//
+		// load the FS redirector function dynamically
+		//
+		hKernel32Dll = LoadLibrary("kernel32.dll");
+
+		if (hKernel32Dll == NULL)
+		{
+			return FALSE;
+		}
+		
+		DisableFsRedirector = 
+			(Wow64DisableWow64FsRedirectionHandler)GetProcAddress(
+				hKernel32Dll, 
+				"Wow64DisableWow64FsRedirection");
+
+		RevertFsRedirector = 
+			(Wow64RevertWow64FsRedirectionHandler)GetProcAddress(
+				hKernel32Dll, 
+				"Wow64RevertWow64FsRedirection");
+
+		if (DisableFsRedirector == NULL || RevertFsRedirector == NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+			return FALSE;
+		}
+
+		if (DisableFsRedirector(&OldFsRedirectorValue) == FALSE)
+		{
+			FreeLibrary(hKernel32Dll);
+			return FALSE;
+		}
+	}
+
+	sprintf(ResName, "#%d", ResID);
 	// 
 	// Find the resource
 	//
 	HRSRC hRes = FindResource(hInst, ResName, RT_RCDATA);
 	if(!hRes)
 	{
+		if (hKernel32Dll != NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+		}
 		return FALSE;
 	}
 
@@ -45,6 +74,10 @@ BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName)
 	DWORD dwResSize	= SizeofResource(hInst, hRes);
 	if(dwResSize == 0)
 	{
+		if (hKernel32Dll != NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+		}
 		return FALSE;
 	}
 
@@ -54,6 +87,10 @@ BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName)
 	HGLOBAL hResGlobal = LoadResource(hInst, hRes);
 	if(!hResGlobal)
 	{
+		if (hKernel32Dll != NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+		}
 		return FALSE;
 	}
 
@@ -64,6 +101,10 @@ BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName)
 	LPVOID lpResMem	= LockResource(hResGlobal);
 	if(!hRes)
 	{
+		if (hKernel32Dll != NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+		}
 		return FALSE;
 	}
 
@@ -71,8 +112,28 @@ BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName)
 	// Save the resource to disk
 	//
 	OutFile = fopen(FileName, "wb");
+
+	if (bDisableFsRedirector)
+	{
+
+		if (RevertFsRedirector(OldFsRedirectorValue) == FALSE)
+		{
+			if (hKernel32Dll != NULL)
+			{
+				FreeLibrary(hKernel32Dll);
+			}
+			return FALSE;
+		}
+	}
+
+
 	if(!OutFile)
 	{
+		if (hKernel32Dll != NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+		}
+
 		return FALSE;
 	}
 
@@ -82,7 +143,18 @@ BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName)
 		OutFile) != dwResSize)
 	{
 		fclose(OutFile);
+
+		if (hKernel32Dll != NULL)
+		{
+			FreeLibrary(hKernel32Dll);
+		}
+
 		return FALSE;
+	}
+
+	if (hKernel32Dll != NULL)
+	{
+		FreeLibrary(hKernel32Dll);
 	}
 
 	fclose(OutFile);
@@ -138,4 +210,3 @@ BOOL WoemGetResource(HINSTANCE hInst, int ResID, LPVOID * lpResMem, LPDWORD dwRe
 
 	return TRUE;
 }
-
