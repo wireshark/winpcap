2e52de525b2fc1d4a20462d8e56101cfcebca206
Gianluca Varenni <gianluca.varenni@cacetech.com>
2006-07-14 16:58:08 +0000

    Updated the version of OEM WinPcap.

--- version.h
+++ version.h
@@ -12,17 +12,17 @@
 // 0 Nov = 181 + (Jul) 31 + (Aug) 31 + (Sept)30 + (Oct)31 = 324
 // 0 Dec = 324 + 30 = 334
 
-#define WINPCAPOEM_MAJOR	3
-#define WINPCAPOEM_MINOR	2
+#define WINPCAPOEM_MAJOR	4
+#define WINPCAPOEM_MINOR	0
 #define WINPCAPOEM_REV		2
-#define WINPCAPOEM_BUILD	473
+#define WINPCAPOEM_BUILD	480
 
-#define WINPCAPOEM_VER_STRING	"3.2.2.473.oem"
+#define WINPCAPOEM_VER_STRING	"4.0.2.480.oem"
 
 #define WINPCAPOEM_COMPANY_NAME 			"CACE Technologies"
 
 #define WINPCAPOEM_PACKET_WRAPPER_NAME 		"Packet.dll Wrapper"
-#define WINPCAPOEM_VERSION_STRING 			"3.2.2.473.oem"
+#define WINPCAPOEM_VERSION_STRING 			"4.0.2.480.oem"
 #define WINPCAPOEM_PRODUCT_NAME 			"OEM WinPcap"
 #define WINPCAPOEM_COPYRIGHT_STRING 		"Copyright © 2005-2006 CACE Technologies"
 #define WINPCAPOEM_PACKET_WRAPPER_FILENAME 	"Packet.dll (wrapper)"
