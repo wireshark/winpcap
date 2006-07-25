6d4f850f88942dc509e6f1b7c1b2c0cfac2eddf7
Gianluca Varenni <gianlucav@winpcap.org>
2006-07-25 00:41:49 +0000

    Added a global version header file that is used for all the modules of WinPcap.

---
+++ version.h
@@ -0,0 +1,31 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Developer Studio generated include file.
+//
+
+// 0 Feb = 31 + 0 = 31
+// 0 Mar = 31 + 28 + 0 = 59
+// o Apr = 31 + 28 + 31 = 90
+// 0 May = 31 + 28 + 31 + 30 = 120
+// 0 Jun = 31 + 28 + 31 + 30 + 31 = 151 
+// 0 Jul = 31 + 28 + 31 + 30 + 31 + 30 = 181
+// 0 Nov = 181 + (Jul) 31 + (Aug) 31 + (Sept)30 + (Oct)31 = 324
+// 0 Dec = 324 + 30 = 334
+
+#define WINPCAP_MAJOR	4
+#define WINPCAP_MINOR	0
+#define WINPCAP_REV		2
+#define WINPCAP_BUILD	480
+#define WINPCAP_VER_STRING	"4.0.2.480"
+#define WINPCAP_PACKET9x_STRING_VERSION	"4.0 beta1"
+
+#define WINPCAP_COMPANY_NAME 			"CACE Technologies"
+
+#define WINPCAP_PRODUCT_NAME 			"WinPcap"
+
+#define WINPCAP_COPYRIGHT_STRING 		"Copyright © 2005-2006 CACE Technologies. Copyright © 1999-2005 NetGroup, Politecnico di Torino."
+#define WINPCAP_WANPACKET_COPYRIGHT_STRING "Copyright © 2005-2006 CACE Technologies. Copyright © 2003-2005 NetGroup, Politecnico di Torino."
+#define WINPCAP_INSTALLERHELPER_COPYRIGHT_STRING "Copyright © 2006 CACE Technologies."
+
+#define WINPCAP_BUILD_DESCRIPTION 		""
+#define WINPCAP_PRIVATE_BUILD			""
+#define WINPCAP_LIBPCAP_VERSION			"0.9.4"
