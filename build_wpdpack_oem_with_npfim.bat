7638fa9e92a9cbe3088bdd9939e3af76d22f9d48
Gianluca Varenni <gianluca.varenni@cacetech.com>
2006-11-22 19:05:58 +0000

    Added a script for the generation of a devpack including the NpfIm loader extensions, the NpfIm binaries, and the NpfImLoader documentation.

---
+++ build_wpdpack_oem_with_npfim.bat
@@ -0,0 +1,30 @@
+@echo off
+
+
+rem call build_wpdpack_oem.bat %1
+
+if "%1"=="" (set __DEST_FOLDER=.\WpdPack\)  ELSE (set __DEST_FOLDER=%1)
+
+rem now we copy the certified files from the NpfIm engine
+
+echo Copying NpfIm specific files
+pause
+
+rem NOTE: we do not copy all the lib/h files, only the ones really needed,
+rem i.e. the ones for the loader
+xcopy /v /Y  ..\NpfIm_DevPack\NpfImLoader.h		%__DEST_FOLDER%\include\ >nul
+xcopy /v /Y  ..\NpfIm_DevPack\NetCfgx.h		%__DEST_FOLDER%\include\ >nul
+xcopy /v /Y  ..\NpfIm_DevPack\NpfImLoader.lib		%__DEST_FOLDER%\lib\ >nul
+
+rem NOTE: but we copy all the binaries...
+xcopy /v /Y  ..\NpfIm_DevPack\*.dll		%__DEST_FOLDER%\bin\ >nul
+
+rem copy the available sample, IfListNpfIm. We assume that the sample is in ..\NpfIm\Sample
+
+mkdir %__DEST_FOLDER%\Examples-Remote\IfListNpfIm >nul 2>nul
+xcopy /v /Y ..\NpfIm\Samples\IfListNpfIm\*.c* %__DEST_FOLDER%\Examples-Remote\IfListNpfIm\ > nul
+xcopy /v /Y ..\NpfIm\Samples\IfListNpfIm\*.h* %__DEST_FOLDER%\Examples-Remote\IfListNpfIm\ > nul
+xcopy /v /Y ..\NpfIm\Samples\IfListNpfIm\*.vcproj %__DEST_FOLDER%\Examples-Remote\IfListNpfIm\ > nul
+xcopy /v /Y ..\NpfIm\Samples\IfListNpfIm\*.sln %__DEST_FOLDER%\Examples-Remote\IfListNpfIm\ > nul
+
+set __DEST_FOLDER=
