a198b035cf8fb8fab6b019ba18e7ec1ec83b11c2
Gianluca Varenni <gianlucav@winpcap.org>
2006-11-27 22:47:03 +0000

    Moved from copy to xcopy, as it reports errors in case of missing files.

--- install/copy_installer_files.bat
+++ install/copy_installer_files.bat
@@ -9,35 +9,42 @@ rem mkdir installer\distribution\9x		 2>nul >nul
 mkdir installer\distribution\x86-64	 2>nul >nul
 mkdir installer\distribution\Vista_x86	 2>nul >nul
 
-copy ..\packetNtx\driver\bin\2k\i386\npf.sys installer\distribution\2000\ >nul
-copy ..\packetNtx\driver\bin\2k\i386\npf.pdb installer\distribution\2000\ >nul
+xcopy /v /Y ..\packetNtx\driver\bin\2k\i386\npf.sys installer\distribution\2000\ >nul
+xcopy /v /Y  ..\packetNtx\driver\bin\2k\i386\npf.pdb installer\distribution\2000\ >nul
 
-copy ..\packetNtx\driver\bin\NT4\i386\free\npf.sys installer\distribution\NT\ >nul
+xcopy /v /Y  ..\packetNtx\driver\bin\NT4\i386\free\npf.sys installer\distribution\NT\ >nul
 
-copy ..\packetNtx\driver\bin\xp\amd64\npf.sys installer\distribution\x86-64\ >nul
-copy ..\packetNtx\driver\bin\xp\amd64\npf.pdb installer\distribution\x86-64\ >nul
+xcopy /v /Y  ..\packetNtx\driver\bin\xp\amd64\npf.sys installer\distribution\x86-64\ >nul
+xcopy /v /Y  ..\packetNtx\driver\bin\xp\amd64\npf.pdb installer\distribution\x86-64\ >nul
 
-copy ..\packetNtx\Dll\Project\Release\Packet.dll installer\distribution\2000\ >nul
-copy ..\packetNtx\Dll\Project\Release_Vista\Packet.dll installer\distribution\Vista_x86\ >nul
-copy ..\packetNtx\Dll\Project\NT4_Release\Packet.dll installer\distribution\NT\ >nul
-copy ..\packetNtx\Dll\WanPacket\Release\WanPacket.dll installer\distribution\2000\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\Project\Release\Packet.dll installer\distribution\2000\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\Project\Release_Vista\Packet.dll installer\distribution\Vista_x86\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\Project\NT4_Release\Packet.dll installer\distribution\NT\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\WanPacket\Release\WanPacket.dll installer\distribution\2000\ >nul
+
+xcopy /v /Y  ..\packetNtx\Dll\Project\Release\Packet.pdb installer\distribution\2000\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\Project\Release_Vista\Packet.pdb installer\distribution\Vista_x86\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\Project\NT4_Release\Packet.pdb installer\distribution\NT\ >nul
+xcopy /v /Y  ..\packetNtx\Dll\WanPacket\Release\WanPacket.pdb installer\distribution\2000\ >nul
 
 rem We do no longer support Win9x
 rem echo REMEMBER TO COPY npf.vxd
 rem pause
 rem copy ..\packet9x\DLL\Project\Release\Packet.dll installer\distribution\9x\
 
-copy ..\wpcap\PRJ\Release_REMOTE\wpcap.dll installer\distribution\ >nul
-copy ..\wpcap\PRJ\Release_REMOTE\rpcapd.exe installer\distribution\2000\ >nul
+xcopy /v /Y  ..\wpcap\PRJ\Release_REMOTE\wpcap.dll installer\distribution\ >nul
+xcopy /v /Y  ..\wpcap\PRJ\Release_REMOTE\rpcapd.exe installer\distribution\2000\ >nul
+xcopy /v /Y  ..\wpcap\PRJ\Release_REMOTE\wpcap.pdb installer\distribution\ >nul
+xcopy /v /Y  ..\wpcap\PRJ\Release_REMOTE\rpcapd.pdb installer\distribution\2000\ >nul
 
 
 rem copy daemon_mgm\Release\daemon_mgm.exe installer\distribution\2000\
 rem copy NetMonInstaller\objfre_w2k_x86\i386\NetMonInstaller.exe installer\distribution\2000\
 rem copy npf_mgm\Release\npf_mgm.exe installer\distribution\2000\
 
-copy "WinPcap Installer Helper\Release\WinPcapInstall.dll" installer\distribution\2000\ >nul
+xcopy /v /Y  "WinPcap Installer Helper\Release\WinPcapInstall.dll" installer\distribution\2000\ >nul
 
-copy ..\wpcap\libpcap\rpcapd\win32-pthreads\pthreadVC.dll installer\distribution\ >nul
+xcopy /v /Y  ..\wpcap\libpcap\rpcapd\win32-pthreads\pthreadVC.dll installer\distribution\ >nul
 rem this nasty hack is needed to fix some time zone issues with the CVS...
 touch installer\distribution\pthreadVC.dll >nul
 
