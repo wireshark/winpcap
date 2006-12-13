4be9c5e9b3eae207248351ba7be83aa90354d9b9
Gianluca Varenni <gianluca.varenni@cacetech.com>
2006-12-13 19:35:58 +0000

    Fixed a typo in a comment.

--- OEM/WinpcapOem/postbuild_release.bat
+++ OEM/WinpcapOem/postbuild_release.bat
@@ -1,6 +1,6 @@
 @echo off
 
 echo Copying dll files for debug
-REM This copies are needed to debug all the stuff internally, in the cpcap output folders
+REM This copies are needed to debug all the stuff internally, in the winpcap output folders
 
 if exist release\packet.lib xcopy release\packet.lib ..\..\..\winpcap\common\ /v /y /q
