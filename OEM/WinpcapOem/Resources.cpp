9f748342c584ebb8cb57d81c55293d6cdf7f0ee3
Gianluca Varenni <gianluca.varenni@cacetech.com>
2006-12-14 19:22:50 +0000

    Migrated from FILE based operations to the Win32 ones (CreateFile, ReadFile and such). Added some better tracing. got rid of the dependency over stdio.h.

--- OEM/WinpcapOem/Resources.cpp
+++ OEM/WinpcapOem/Resources.cpp
@@ -1,6 +1,7 @@
 #include <windows.h>
-#include <stdio.h>
+#include <strsafe.h>
 #include "WinpcapOem.h"
+#include "WoemDebug.h"
 
 ////////////////////////////////////////////////////////////////////
 // This function locates one of the binary resources embedded in the
@@ -8,205 +9,218 @@
 ////////////////////////////////////////////////////////////////////
 BOOL WoemSaveResourceToDisk(HINSTANCE hInst, int ResID, char* FileName, BOOL bDisableFsRedirector)
 {
-	char ResName[100];
-	FILE* OutFile;
-	PVOID OldFsRedirectorValue;
+	DWORD dwLastError = ERROR_SUCCESS;
+	HANDLE hFile = INVALID_HANDLE_VALUE;
+	PVOID OldFsRedirectorValue = NULL;
 	HMODULE hKernel32Dll = NULL;
-	Wow64DisableWow64FsRedirectionHandler DisableFsRedirector;
-	Wow64RevertWow64FsRedirectionHandler RevertFsRedirector;
+	Wow64DisableWow64FsRedirectionHandler DisableFsRedirector = NULL;
+	Wow64RevertWow64FsRedirectionHandler RevertFsRedirector = NULL;
+	CHAR ResName[100];
 
 #ifdef STATIC_LIB
 	hInst = NULL;
 #endif
 
-	if (bDisableFsRedirector)
-	{
-		//
-		// load the FS redirector function dynamically
-		//
-		hKernel32Dll = LoadLibrary("kernel32.dll");
+	TRACE_ENTER("WoemSaveResourceToDisk");
 
-		if (hKernel32Dll == NULL)
+	StringCchPrintfA(ResName, sizeof(ResName), "#%d", ResID);
+
+	TRACE_MESSAGE("Decompressing to file %s", FileName);
+
+	do
+	{
+		if (bDisableFsRedirector)
 		{
-			return FALSE;
-		}
-		
-		DisableFsRedirector = 
-			(Wow64DisableWow64FsRedirectionHandler)GetProcAddress(
+			//
+			// load the FS redirector function dynamically
+			//
+			hKernel32Dll = LoadLibrary("kernel32.dll");
+
+			if (hKernel32Dll == NULL)
+			{
+				dwLastError = GetLastError();
+				TRACE_MESSAGE("Cannot load kernel32.dll to disable the FS redirector");
+				break;
+			}
+
+			DisableFsRedirector = 
+				(Wow64DisableWow64FsRedirectionHandler)GetProcAddress(
 				hKernel32Dll, 
 				"Wow64DisableWow64FsRedirection");
 
-		RevertFsRedirector = 
-			(Wow64RevertWow64FsRedirectionHandler)GetProcAddress(
+			RevertFsRedirector = 
+				(Wow64RevertWow64FsRedirectionHandler)GetProcAddress(
 				hKernel32Dll, 
 				"Wow64RevertWow64FsRedirection");
 
-		if (DisableFsRedirector == NULL || RevertFsRedirector == NULL)
-		{
-			FreeLibrary(hKernel32Dll);
-			return FALSE;
-		}
-
-		if (DisableFsRedirector(&OldFsRedirectorValue) == FALSE)
-		{
-			FreeLibrary(hKernel32Dll);
-			return FALSE;
-		}
-	}
-
-	sprintf(ResName, "#%d", ResID);
-	// 
-	// Find the resource
-	//
-	HRSRC hRes = FindResource(hInst, ResName, RT_RCDATA);
-	if(!hRes)
-	{
-		if (hKernel32Dll != NULL)
-		{
-			FreeLibrary(hKernel32Dll);
-		}
-		return FALSE;
-	}
-
-	// 
-	// Get the size of the resource
-	//
-	DWORD dwResSize	= SizeofResource(hInst, hRes);
-	if(dwResSize == 0)
-	{
-		if (hKernel32Dll != NULL)
-		{
-			FreeLibrary(hKernel32Dll);
-		}
-		return FALSE;
-	}
-
-	// 
-	// Load the resource
-	//
-	HGLOBAL hResGlobal = LoadResource(hInst, hRes);
-	if(!hResGlobal)
-	{
-		if (hKernel32Dll != NULL)
-		{
-			FreeLibrary(hKernel32Dll);
-		}
-		return FALSE;
-	}
-
-	// 
-	// Get the address of the resource
-	// Note: according to MSDN it's not necessary to unlock the resource
-	//
-	LPVOID lpResMem	= LockResource(hResGlobal);
-	if(!hRes)
-	{
-		if (hKernel32Dll != NULL)
-		{
-			FreeLibrary(hKernel32Dll);
-		}
-		return FALSE;
-	}
-
-	//
-	// Save the resource to disk
-	//
-	OutFile = fopen(FileName, "wb");
-
-	if (bDisableFsRedirector)
-	{
-
-		if (RevertFsRedirector(OldFsRedirectorValue) == FALSE)
-		{
-			if (hKernel32Dll != NULL)
+			if (DisableFsRedirector == NULL || RevertFsRedirector == NULL)
 			{
-				FreeLibrary(hKernel32Dll);
+				dwLastError = ERROR_OUTOFMEMORY;
+				break;
 			}
-			return FALSE;
+
+			if (DisableFsRedirector(&OldFsRedirectorValue) == FALSE)
+			{
+				dwLastError = GetLastError();
+				break;
+			}
 		}
-	}
 
-
-	if(!OutFile)
-	{
-		if (hKernel32Dll != NULL)
+		// 
+		// Find the resource
+		//
+		HRSRC hRes = FindResource(hInst, ResName, RT_RCDATA);
+		if(hRes == NULL)
 		{
-			FreeLibrary(hKernel32Dll);
+			dwLastError = GetLastError();
+			break;		
 		}
 
-		return FALSE;
-	}
-
-	if(fwrite(lpResMem,
-		1,
-		dwResSize,
-		OutFile) != dwResSize)
-	{
-		fclose(OutFile);
-
-		if (hKernel32Dll != NULL)
+		// 
+		// Get the size of the resource
+		//
+		DWORD dwResSize	= SizeofResource(hInst, hRes);
+		if(dwResSize == 0)
 		{
-			FreeLibrary(hKernel32Dll);
+			dwLastError = GetLastError();
+			break;
 		}
 
-		return FALSE;
-	}
+		TRACE_MESSAGE("The resource has a size of %u bytes", dwResSize);
+		// 
+		// Load the resource
+		//
+		HGLOBAL hResGlobal = LoadResource(hInst, hRes);
+		if( hResGlobal == NULL)
+		{
+			dwLastError = GetLastError();
+			break;
+		}
 
-	if (hKernel32Dll != NULL)
-	{
-		FreeLibrary(hKernel32Dll);
-	}
+		// 
+		// Get the address of the resource
+		// Note: according to MSDN it's not necessary to unlock the resource
+		//
+		LPVOID lpResMem	= LockResource(hResGlobal);
+		if(!hRes)
+		{
+			dwLastError = GetLastError();
+			break;
+		}
 
-	fclose(OutFile);
+		hFile = CreateFile(FileName, GENERIC_WRITE, 0, NULL,  OPEN_ALWAYS, 0, NULL);
 
-	return TRUE;
+		if (hFile == INVALID_HANDLE_VALUE)
+		{
+			dwLastError = GetLastError();
+			TRACE_MESSAGE("Cannot open file %8.8x", dwLastError);
+		}
+
+		//
+		// Save the resource to disk
+		//
+		if (bDisableFsRedirector)
+		{
+			if (RevertFsRedirector(OldFsRedirectorValue) == FALSE)
+			{
+				dwLastError = GetLastError();
+				break;
+			}
+		}
+
+		if (hFile == INVALID_HANDLE_VALUE)
+		{
+			break;
+		}
+	
+		DWORD writtenBytes = 0;
+		do
+		{
+			DWORD localWrittenBytes;
+
+			if (WriteFile(hFile, lpResMem, dwResSize - writtenBytes, &localWrittenBytes, NULL) == FALSE)
+			{
+				dwLastError = GetLastError();
+				break;
+			}
+			TRACE_MESSAGE("Written %u bytes", localWrittenBytes);
+			writtenBytes += localWrittenBytes;
+		}
+		while(writtenBytes < dwResSize);
+		
+	}while(FALSE);
+
+	if (hKernel32Dll != NULL) FreeLibrary(hKernel32Dll);
+
+	if (hFile != INVALID_HANDLE_VALUE) CloseHandle(hFile);
+
+	if (hFile != INVALID_HANDLE_VALUE && dwLastError != ERROR_SUCCESS) DeleteFileA(FileName);
+
+	if (dwLastError != ERROR_SUCCESS) SetLastError(dwLastError);
+
+	TRACE_EXIT("WoemSaveResourceToDisk");
+
+	return (dwLastError == ERROR_SUCCESS);
 }
 
 BOOL WoemGetResource(HINSTANCE hInst, int ResID, LPVOID * lpResMem, LPDWORD dwResSize)
 {
 	char ResName[100];
-	sprintf(ResName, "#%d", ResID);
+	HRSRC hRes;
+	BOOL bResult = FALSE;
+
+	TRACE_ENTER("WoemGetResource");
 
 #ifdef STATIC_LIB
 	hInst = NULL;
 #endif
 
-	// 
-	// Find the resource
-	//
-	HRSRC hRes = FindResource(hInst, ResName, RT_RCDATA);
-	if(!hRes)
-	{
-		return FALSE;
-	}
+	StringCchPrintfA(ResName, sizeof(ResName), "#%d", ResID);
 
-	// 
-	// Get the size of the resource
-	//
-	*dwResSize	= SizeofResource(hInst, hRes);
-	if(dwResSize == 0)
+	do
 	{
-		return FALSE;
-	}
 
-	// 
-	// Load the resource
-	//
-	HGLOBAL hResGlobal = LoadResource(hInst, hRes);
-	if(!hResGlobal)
-	{
-		return FALSE;
-	}
+		// 
+		// Find the resource
+		//
+		hRes = FindResource(hInst, ResName, RT_RCDATA);
+		if(!hRes)
+		{
+			break;
+		}
 
-	// 
-	// Get the address of the resource
-	// Note: according to MSDN it's not necessary to unlock the resource
-	//
-	*lpResMem = LockResource(hResGlobal);
-	if(!hRes)
-	{
-		return FALSE;
-	}
+		// 
+		// Get the size of the resource
+		//
+		*dwResSize	= SizeofResource(hInst, hRes);
+		if(dwResSize == 0)
+		{
+			break;
+		}
+	
+		// 
+		// Load the resource
+		//
+		HGLOBAL hResGlobal = LoadResource(hInst, hRes);
+		if(!hResGlobal)
+		{
+			break;
+		}
 
-	return TRUE;
+		// 
+		// Get the address of the resource
+		// Note: according to MSDN it's not necessary to unlock the resource
+		//
+		*lpResMem = LockResource(hResGlobal);
+		if(!hRes)
+		{
+			break;
+		}
+	
+		bResult = TRUE;
+	}while(FALSE);
+
+	TRACE_EXIT("WoemGetResource");
+
+	return bResult;
 }
