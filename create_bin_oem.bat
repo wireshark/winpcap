e9dedcb69f00dcbdf6345d2c06fd1c58bbdde196
Gianluca Varenni <gianluca.varenni@cacetech.com>
2007-01-22 23:16:10 +0000

    Modified the script to copy the signed binaries if possible.

--- create_bin_oem.bat
+++ create_bin_oem.bat
@@ -6,8 +6,19 @@ echo Creating \bin folder
 mkdir %WPDPACKDESTDIR% >nul 2>nul
 mkdir %WPDPACKDESTDIR%\bin >nul 2>nul
 
-xcopy /v /Y .\wpcap\PRJ\Release_REMOTE\wpcap.dll			%WPDPACKDESTDIR%\bin\ >nul
-xcopy /v /Y .\OEM\WinPcapOEM\release\packet.dll				%WPDPACKDESTDIR%\bin\ >nul
+if not exist .\oem\WinPcapOem\Binaries_SIGNED\wpcap.dll (
+	echo Not using the signed version of the DLLs!!
+	xcopy /v /Y .\wpcap\PRJ\Release_REMOTE\wpcap.dll	%WPDPACKDESTDIR%\bin\ >nul		
+) else (
+	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\wpcap.dll %WPDPACKDESTDIR%\bin\ >nul
+)
+
+if not exist .\oem\WinPcapOem\Binaries_signed\packet.dll (
+	echo Not using the signed version of the DLLs!!
+	xcopy /v /Y .\OEM\WinPcapOEM\release\packet.dll				%WPDPACKDESTDIR%\bin\ >nul
+) else (
+	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\packet.dll %WPDPACKDESTDIR%\bin\ >nul
+)
 
 echo Folder \bin created successfully
 
