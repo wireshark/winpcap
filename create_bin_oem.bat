adc2f4579cb4aa20994974a0d1b92bcd3bd3267c
Gianluca Varenni <gianluca.varenni@cacetech.com>
2007-01-22 23:40:31 +0000

    Added some messages to detail which version of the DLLs has been used.

--- create_bin_oem.bat
+++ create_bin_oem.bat
@@ -7,16 +7,18 @@ mkdir %WPDPACKDESTDIR% >nul 2>nul
 mkdir %WPDPACKDESTDIR%\bin >nul 2>nul
 
 if not exist .\oem\WinPcapOem\Binaries_SIGNED\wpcap.dll (
-	echo Not using the signed version of the DLLs!!
+	echo Not using the signed version of the wpcap.dll
 	xcopy /v /Y .\wpcap\PRJ\Release_REMOTE\wpcap.dll	%WPDPACKDESTDIR%\bin\ >nul		
 ) else (
+	echo Using the signed version of wpcap.dll
 	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\wpcap.dll %WPDPACKDESTDIR%\bin\ >nul
 )
 
 if not exist .\oem\WinPcapOem\Binaries_signed\packet.dll (
-	echo Not using the signed version of the DLLs!!
+	echo Not using the signed version of packet.dll
 	xcopy /v /Y .\OEM\WinPcapOEM\release\packet.dll				%WPDPACKDESTDIR%\bin\ >nul
 ) else (
+	echo Using the signed version of packet.dll
 	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\packet.dll %WPDPACKDESTDIR%\bin\ >nul
 )
 
