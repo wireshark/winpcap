295aebe920f3e4b19bfebb07db1d6e3133f6eac0
Gianluca Varenni <gianluca.varenni@cacetech.com>
2008-06-03 22:46:47 +0000

    Documented a new flag for pcap_start_oem

--- wpcap/pcap_oem.c
+++ wpcap/pcap_oem.c
@@ -6,13 +6,21 @@
 //\{ 
 
 /*!
+	\ingroup wpcapfunc
+	\brief Flag used in conjunction with \ref pcap_start_oem.
+	When this flag is used, WinPcap Professional will be started without support
+	for dialup and VPN adapters (i.e. without loading the Microsoft NetMon component).
+*/
+#define PCAP_START_OEM_NO_NETMON	0x00000001
+
+/*!
 	\ingroup wpcapfunc 
 	\brief This function starts WinPcap Professional.
 	
 	\param err_str pointer to a user-allocated buffer (of minimum size PCAP_ERRBUF_SIZE) 
 		that will contain the error message in case of failure.
-	\param flags Reserved. It should be set to 0.
-
+	\param flags A bitwise combination of the following flags:
+		- \ref PCAP_START_OEM_NO_NETMON
 	\return 0 on success,  -1 in case of failure.
 */
 int pcap_start_oem(char* err_str, int flags)
