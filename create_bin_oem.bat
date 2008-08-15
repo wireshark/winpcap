bfd022bd37b97220538a7879d10d4c1d010d4249
Gianluca Varenni <gianluca.varenni@cacetech.com>
2008-08-15 00:00:54 +0000

    Copy the x64 binaries. Updated the paths to the binaries after migrating to VS2005.

--- create_bin_oem.bat
+++ create_bin_oem.bat
@@ -5,22 +5,30 @@ IF "%1"=="" (set WPDPACKDESTDIR=.\WpdPack\) ELSE (set WPDPACKDESTDIR=%1)
 echo Creating \bin folder
 mkdir %WPDPACKDESTDIR% >nul 2>nul
 mkdir %WPDPACKDESTDIR%\bin >nul 2>nul
+mkdir %WPDPACKDESTDIR%\bin\x64 >nul 2>nul
+mkdir %WPDPACKDESTDIR%\bin\x86 >nul 2>nul
 
-if not exist .\oem\WinPcapOem\Binaries_SIGNED\wpcap.dll (
-	echo Not using the signed version of the wpcap.dll
-	xcopy /v /Y .\wpcap\PRJ\Release_REMOTE\wpcap.dll	%WPDPACKDESTDIR%\bin\ >nul		
-) else (
-	echo Using the signed version of wpcap.dll
-	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\wpcap.dll %WPDPACKDESTDIR%\bin\ >nul
-)
+rem if not exist .\oem\WinPcapOem\Binaries_SIGNED\wpcap.dll (
+rem	echo Not using the signed version of the wpcap.dll
+rem	xcopy /v /Y .\wpcap\PRJ\Release_REMOTE\wpcap.dll	%WPDPACKDESTDIR%\bin\ >nul		
+rem ) else (
+rem	echo Using the signed version of wpcap.dll
+rem	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\wpcap.dll %WPDPACKDESTDIR%\bin\ >nul
+rem )
+rem 
+rem if not exist .\oem\WinPcapOem\Binaries_signed\packet.dll (
+rem	echo Not using the signed version of packet.dll
+rem	xcopy /v /Y .\OEM\WinPcapOEM\release\packet.dll				%WPDPACKDESTDIR%\bin\ >nul
+rem ) else (
+rem	echo Using the signed version of packet.dll
+rem	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\packet.dll %WPDPACKDESTDIR%\bin\ >nul
+rem )
 
-if not exist .\oem\WinPcapOem\Binaries_signed\packet.dll (
-	echo Not using the signed version of packet.dll
-	xcopy /v /Y .\OEM\WinPcapOEM\release\packet.dll				%WPDPACKDESTDIR%\bin\ >nul
-) else (
-	echo Using the signed version of packet.dll
-	xcopy /v /y .\oem\WinPcaPOem\binaries_signed\packet.dll %WPDPACKDESTDIR%\bin\ >nul
-)
+
+xcopy /v /y ".\wpcap\PRJ\Release REMOTE\x86\wpcap.dll" %WPDPACKDESTDIR%\bin\x86\ >nul
+xcopy /v /y ".\wpcap\PRJ\Release REMOTE\x64\wpcap.dll" %WPDPACKDESTDIR%\bin\x64\ >nul
+xcopy /v /y ".\OEM\WinpcapOem\Release\x86\packet.dll"  %WPDPACKDESTDIR%\bin\x86\ >nul
+xcopy /v /y ".\OEM\WinpcapOem\Release\x64\packet.dll"  %WPDPACKDESTDIR%\bin\x64\ >nul
 
 echo Folder \bin created successfully
 
--- create_lib_oem.bat
+++ create_lib_oem.bat
@@ -5,9 +5,12 @@ IF "%1"=="" (set WPDPACKDESTDIR=.\WpdPack\) ELSE (set WPDPACKDESTDIR=%1)
 echo Creating \Lib folder
 mkdir %WPDPACKDESTDIR% >nul 2>nul
 mkdir %WPDPACKDESTDIR%\Lib >nul 2>nul
+mkdir %WPDPACKDESTDIR%\Lib\x64 >nul 2>nul
 
-xcopy /v /Y .\wpcap\PRJ\Release_REMOTE\wpcap.lib			%WPDPACKDESTDIR%\Lib\  >nul
-xcopy /v /Y .\OEM\WinPcapOEM\release\packet.lib				%WPDPACKDESTDIR%\Lib\ 	>nul
+xcopy /v /Y ".\wpcap\PRJ\Release REMOTE\x86\wpcap.lib"			%WPDPACKDESTDIR%\Lib\  >nul
+xcopy /v /Y ".\wpcap\PRJ\Release REMOTE\x64\wpcap.lib"			%WPDPACKDESTDIR%\Lib\x64\  >nul
+xcopy /v /Y .\OEM\WinPcapOEM\release\x86\packet.lib				%WPDPACKDESTDIR%\Lib\ 	>nul
+xcopy /v /Y .\OEM\WinPcapOEM\release\x64\packet.lib				%WPDPACKDESTDIR%\Lib\x64\ 	>nul
 
 echo Folder \Lib created successfully
 
