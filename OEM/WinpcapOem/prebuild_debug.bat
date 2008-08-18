a367e8dec8f40cffd5e3c05e25113f0b343ed09f
Gianluca Varenni <gianluca.varenni@cacetech.com>
2008-08-18 22:37:20 +0000

    Create the output folders if they don't exist.

--- OEM/WinpcapOem/prebuild_debug.bat
+++ OEM/WinpcapOem/prebuild_debug.bat
@@ -3,6 +3,10 @@
 echo Copying Debug DLLs
 
 if "%1"=="x64" (
+echo Creating folders
+mkdir .\Binaries\ > NUL 2>NUL
+mkdir .\Binaries\x64 > NUL 2>NUL
+mkdir .\Binaries\x86 > NUL 2>NUL
 echo Copying compiled DLLs for x64
 copy "..\..\packetNtx\Dll\Project\Packet (NetMon) Debug\x64\packet.dll"		.\Binaries\x64\packet2k.dll
 copy "..\..\PacketNtx\Dll\Project\Packet (NoNetMon) Debug\x64\packet.dll"	.\Binaries\x64\packetVista.dll
@@ -11,6 +15,10 @@ copy ..\..\packetNtx\Driver\bin\xp\AMD64\npf.sys							.\Binaries\x64\npfx64.sys
 )
 
 if "%1"=="x86" (
+echo Creating folders
+mkdir .\Binaries\ > NUL 2>NUL
+mkdir .\Binaries\x64 > NUL 2>NUL
+mkdir .\Binaries\x86 > NUL 2>NUL
 echo Copying compiled DLLs for x86
 copy "..\..\packetNtx\Dll\Project\Packet (NT4) Debug\x86\packet.dll"		.\Binaries\x86\packetnt.dll
 copy "..\..\packetNtx\Dll\Project\Packet (NetMon) Debug\x86\packet.dll"		.\Binaries\x86\packet2k.dll
--- OEM/WinpcapOem/prebuild_release.bat
+++ OEM/WinpcapOem/prebuild_release.bat
@@ -3,6 +3,10 @@
 echo Copying Release DLLs
 
 if "%1"=="x64" (
+echo Creating folders
+mkdir .\Binaries\ > NUL 2>NUL
+mkdir .\Binaries\x64 > NUL 2>NUL
+mkdir .\Binaries\x86 > NUL 2>NUL
 echo Copying compiled DLLs for x64
 copy "..\..\packetNtx\Dll\Project\Packet (NetMon) Release\x64\packet.dll"	.\Binaries\x64\packet2k.dll
 copy "..\..\PacketNtx\Dll\Project\Packet (NoNetMon) Release\x64\packet.dll"	.\Binaries\x64\packetVista.dll
@@ -11,6 +15,10 @@ copy ..\..\packetNtx\Driver\bin\xp\AMD64\npf.sys							.\Binaries\x64\npfx64.sys
 )
 
 if "%1"=="x86" (
+echo Creating folders
+mkdir .\Binaries\ > NUL 2>NUL
+mkdir .\Binaries\x64 > NUL 2>NUL
+mkdir .\Binaries\x86 > NUL 2>NUL
 echo Copying compiled DLLs for x86
 copy "..\..\packetNtx\Dll\Project\Packet (NT4) Release\x86\packet.dll"		.\Binaries\x86\packetnt.dll
 copy "..\..\packetNtx\Dll\Project\Packet (NetMon) Release\x86\packet.dll"	.\Binaries\x86\packet2k.dll
