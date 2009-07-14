a059415ec1fa90b998896e22f4425022f3473e84
Gianluca Varenni <gianluca.varenni@cacetech.com>
2009-07-14 23:59:08 +0000

    Updated the include files.

--- create_include_oem.bat
+++ create_include_oem.bat
@@ -7,30 +7,27 @@ IF ""=="%1" (set WINPCAPSOURCEDIR=.\) ELSE (set WINPCAPSOURCEDIR=%1)
 echo Creating \Include folder
 mkdir %WPDPACKDESTDIR%  2>nul >nul
 mkdir %WPDPACKDESTDIR%\Include  2>nul >nul
+mkdir %WPDPACKDESTDIR%\Include\pcap  2>nul >nul
 
-xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Include\bittypes.h 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Include\Gnuc.h 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Include\ip6_misc.h		%WPDPACKDESTDIR%\Include\	 >nul
-rem xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\rpcapd\win32-pthreads\pthread.h	%WPDPACKDESTDIR%\Include\	 >nul
-rem xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\rpcapd\win32-pthreads\sched.h 	%WPDPACKDESTDIR%\Include\	 >nul
-rem xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\rpcapd\win32-pthreads\semaphore.h	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\PCAP-INT.H		 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap-bpf.h		 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap-stdinc.h		 	%WPDPACKDESTDIR%\Include\	 >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap\*.h			 	%WPDPACKDESTDIR%\Include\pcap\	 >nul
 xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\PCAP.H			 	%WPDPACKDESTDIR%\Include\	 >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap-bpf.h		 		%WPDPACKDESTDIR%\Include\	 >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap-namedb.h	 		%WPDPACKDESTDIR%\Include\	 >nul
 xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\remote-ext.h		 	%WPDPACKDESTDIR%\Include\	 >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\pcap-stdinc.h		 	%WPDPACKDESTDIR%\Include\	 >nul
 xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\Win32-Extensions\Win32-Extensions.h 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\bucket_lookup.h	 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\count_packets.h	 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\memory_t.h		 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\normal_lookup.h	 		%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\tcp_session.h		 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\time_calls.h		 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\tme.h			 	%WPDPACKDESTDIR%\Include\	 >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Include\bittypes.h 		%WPDPACKDESTDIR%\Include\	 >nul
+xcopy /v /Y %WINPCAPSOURCEDIR%\wpcap\libpcap\Win32\Include\ip6_misc.h		%WPDPACKDESTDIR%\Include\	 >nul
+
 xcopy /v /Y %WINPCAPSOURCEDIR%\Common\Packet32.h			 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\Common\Devioctl.h			 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\Common\Ntddndis.h			 	%WPDPACKDESTDIR%\Include\	 >nul
-xcopy /v /Y %WINPCAPSOURCEDIR%\Common\Ntddpack.h			 	%WPDPACKDESTDIR%\Include\	 >nul
+
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\bucket_lookup.h	 		%WPDPACKDESTDIR%\Include\	 >nul
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\count_packets.h	 		%WPDPACKDESTDIR%\Include\	 >nul
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\memory_t.h		 	%WPDPACKDESTDIR%\Include\	 >nul
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\normal_lookup.h	 		%WPDPACKDESTDIR%\Include\	 >nul
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\tcp_session.h		 	%WPDPACKDESTDIR%\Include\	 >nul
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\time_calls.h		 	%WPDPACKDESTDIR%\Include\	 >nul
+rem xcopy /v /Y %WINPCAPSOURCEDIR%\packetNtx\driver\tme.h			 	%WPDPACKDESTDIR%\Include\	 >nul
 
 echo Folder \Include created successfully
 set WPDPACKDESTDIR=
