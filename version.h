769d8e0e55bf22eec87981429ba52b07f073c7cc
Gianluca Varenni <gianlucav@winpcap.org>
2009-10-20 17:51:22 +0000

    Update version to 4.1.0.1753. (4.1.1)

--- version.h
+++ version.h
@@ -16,14 +16,15 @@
 // 4.1.0.1237 -->  WinPcap  4.1 beta4
 // 4.1.0.1452 -->  WinPcap  4.1 beta5
 // 4.1.0.1752 -->  WinPcap  4.1 RTM
+// 4.1.0.1753 -->  WinPcap  4.1.1 RTM
 
 #define WINPCAP_MAJOR	4
 #define WINPCAP_MINOR	1
 #define WINPCAP_REV		0
-#define WINPCAP_BUILD	1752
-#define WINPCAP_VER_STRING	"4.1.0.1752"
-#define WINPCAP_PACKET9x_STRING_VERSION	"4.1"
-#define WINPCAP_WPCAP_STRING_VERSION "4.1"
+#define WINPCAP_BUILD	1753
+#define WINPCAP_VER_STRING	"4.1.0.1753"
+#define WINPCAP_PACKET9x_STRING_VERSION	"4.1.1"
+#define WINPCAP_WPCAP_STRING_VERSION "4.1.1"
 
 #define WINPCAP_COMPANY_NAME 			"CACE Technologies, Inc."
 
