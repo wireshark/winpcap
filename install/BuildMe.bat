473405612f0c41be26600ef056253f94381c2f2c
Gianluca Varenni <gianlucav@winpcap.org>
2010-06-22 22:31:26 +0000

    Build the debug versions first.

--- install/BuildMe.bat
+++ install/BuildMe.bat
@@ -1,5 +1,9 @@
 @echo off
 
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug"			/p:Platform="Win32"
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No AirPcap"		/p:Platform="Win32"
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No NetMon"		/p:Platform="Win32"
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug NT4"			/p:Platform="Win32"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release"			/p:Platform="Win32"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release No AirPcap"		/p:Platform="Win32"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release No NetMon"		/p:Platform="Win32"
@@ -7,11 +11,11 @@ msbuild MakeAll.sln /t:Build	/p:Configuration="Release NT4"			/p:Platform="Win32
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release LOG_TO_FILE"		/p:Platform="Win32"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release No NetMon LOG_TO_FILE" /p:Platform="Win32"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release NT4 LOG_TO_FILE"	/p:Platform="Win32"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug"			/p:Platform="Win32"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No AirPcap"		/p:Platform="Win32"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No NetMon"		/p:Platform="Win32"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug NT4"			/p:Platform="Win32"
 
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug"			/p:Platform="x64"
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No AirPcap"		/p:Platform="x64"
+msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No NetMon"		/p:Platform="x64"
+rem msbuild MakeAll.sln /t:Build	/p:Configuration="Debug NT4"			/p:Platform="x64"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release"			/p:Platform="x64"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release No AirPcap"		/p:Platform="x64"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release No NetMon"		/p:Platform="x64"
@@ -19,7 +23,3 @@ rem msbuild MakeAll.sln /t:Build	/p:Configuration="Release NT4"			/p:Platform="x
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release LOG_TO_FILE"		/p:Platform="x64"
 msbuild MakeAll.sln /t:Build	/p:Configuration="Release No NetMon LOG_TO_FILE" /p:Platform="x64"
 rem msbuild MakeAll.sln /t:Build	/p:Configuration="Release NT4 LOG_TO_FILE"	/p:Platform="x64"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug"			/p:Platform="x64"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No AirPcap"		/p:Platform="x64"
-msbuild MakeAll.sln /t:Build	/p:Configuration="Debug No NetMon"		/p:Platform="x64"
-rem msbuild MakeAll.sln /t:Build	/p:Configuration="Debug NT4"			/p:Platform="x64"
