182caa71e8214a89058a8792ed71a975b19a7c2e
Gerald Combs <gerald@wireshark.org>
2015-07-20 23:41:35 +0000

    Copy win-setup.ps1 from Wireshark.
    Copy tools\win-setup.ps1 from the Wireshark repository. Adapt it for
    our uses. Update the license to match libpcap / WinPcap.
    
    Update the release procedures.
    
    Change-Id: Ic10a30c5c5a927dc141a348bd169786cc45dc207
    Reviewed-on: https://code.wireshark.org/review/9730
    Reviewed-by: Gerald Combs <gerald@wireshark.org>

---
+++ tools/win-setup.ps1
@@ -0,0 +1,227 @@
+#
+# win-setup - Prepare a Windows development environment for building WinPcap.
+#
+# Copyright 2015 Gerald Combs <gerald@wireshark.org>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#   1. Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#   2. Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in
+#      the documentation and/or other materials provided with the
+#      distribution.
+#   3. The names of the authors may not be used to endorse or promote
+#      products derived from this software without specific prior
+#      written permission.
+#
+# THIS SOFTWARE IS PROVIDED ``AS IS'' AND WITHOUT ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+
+#requires -version 2
+
+# To do:
+# - NuGet-ize the AirPcap devpack.
+# - Add NSIS ExecDos (site is currently down).
+# - Have a designated libraries directory? We currently use ".."
+
+<#
+.SYNOPSIS
+Prepare a Windows development environment for building WinPcap.
+
+.DESCRIPTION
+This script downloads and extracts third-party libraries required to compile
+WinPcap.
+
+.PARAMETER Destination
+Specifies the destination directory for the downloaded files.
+
+.PARAMETER Force
+Download each library even if exists on the local system.
+
+.INPUTS
+-Destination Destination directory.
+-Force Force fresh downloads.
+
+.OUTPUTS
+A set of libraries required to compile Wireshark on WinPcap, along with
+their compressed archives.
+A date stamp (current-tag.txt)
+
+.EXAMPLE
+C:\PS> .\tools\win-setup.ps1 -Destination .
+#>
+
+Param(
+    [Parameter(Mandatory=$true, Position=0)]
+    [String]
+    $Destination,
+
+    [Parameter(Mandatory=$false)]
+    [Switch]
+    $Force
+)
+
+# Variables
+
+# We create and delete files and directories. Bail out at the first sign of
+# trouble instead of trying to catch exceptions everywhere.
+$ErrorActionPreference = "Stop"
+
+$WinCurrentTag = "2015-05-30"
+
+# Archive file = uncompressed subdirectory.
+$WinArchives = @{
+    "AirPcap_Devpack_4_1_0_1622.zip" = "";
+}
+
+# Plain file downloads
+
+$WinFiles = @(
+)
+
+$Archives = $WinArchives;
+$Files = $WinFiles;
+$CurrentTag = $WinCurrentTag;
+
+$CleanupItems = @(
+    "AirPcap_Devpack_4_1_0_1622"
+    "current-tag.txt"
+)
+
+[Uri] $DownloadPrefix = "https://anonsvn.wireshark.org/wireshark-win32-libs/tags/$($CurrentTag)/packages"
+$Global:SevenZip = "7-zip-not-found"
+
+# Functions
+
+function DownloadFile($fileName, [Uri] $fileUrl = $null) {
+    if ([string]::IsNullOrEmpty($fileUrl)) {
+        $fileUrl = "$DownloadPrefix/$fileName"
+    }
+    $destinationFile = "$fileName"
+    if ((Test-Path $destinationFile -PathType 'Leaf') -and -not ($Force)) {
+        Write-Output "$destinationFile already there; not retrieving."
+        return
+    }
+
+    $proxy = [System.Net.WebRequest]::GetSystemWebProxy()
+    $proxy.Credentials = [System.Net.CredentialCache]::DefaultCredentials
+
+    Write-Output "Downloading $fileUrl into $Destination"
+    $webClient = New-Object System.Net.WebClient
+    $webClient.proxy = $proxy
+    $webClient.DownloadFile($fileUrl, "$Destination\$destinationFile")
+}
+
+# Find 7-Zip, downloading it if necessary.
+# If we ever add NuGet support we might be able to use
+# https://github.com/thoemmi/7Zip4Powershell
+function Bootstrap7Zip() {
+    $searchExes = @("7z.exe", "7za.exe")
+
+    # First, check $env:Path.
+    foreach ($exe in $searchExes) {
+        if (Get-Command $exe -ErrorAction SilentlyContinue)  {
+            $Global:SevenZip = "$exe"
+            return
+        }
+    }
+
+    # Next, look in a few likely places.
+    $searchDirs = @(
+        "${env:ProgramFiles}\7-Zip"
+        "${env:ProgramFiles(x86)}\7-Zip"
+        "${env:ChocolateyInstall}\chocolateyinstall\tools"
+    )
+
+    foreach ($dir in $searchDirs) {
+        if ($dir -ne $null -and (Test-Path $dir -PathType 'Container')) {
+            foreach ($exe in $searchExes) {
+                if (Test-Path "$dir\$exe" -PathType 'Leaf') {
+                    $Global:SevenZip = "$dir\$exe"
+                    return
+                }
+            }
+        }
+    }
+
+    # Finally, download a copy from anonsvn.
+    $binDir = "$Destination"
+
+    [Uri] $bbUrl = "https://anonsvn.wireshark.org/wireshark-win32-libs/trunk/bin/7za.exe"
+    DownloadFile "$binDir\7za.exe" "$bbUrl"
+    Write-Output "Downlaoded $bindir\7za.exe"
+
+    $Global:SevenZip = "$binDir\7za.exe"
+}
+
+function DownloadArchive($fileName, $subDir) {
+    DownloadFile $fileName
+    # $shell = New-Object -com shell.application
+    $archiveFile = "$Destination\$fileName"
+    $archiveDir = "$Destination\$subDir"
+    if ($subDir -and -not (Test-Path $archiveDir -PathType 'Container')) {
+        New-Item -ItemType Directory -Path $archiveDir > $null
+    }
+    $activity = "Extracting into $($archiveDir)"
+    Write-Progress -Activity "$activity" -Status "Running 7z x $archiveFile ..."
+    & "$SevenZip" x "-o$archiveDir" -y "$archiveFile" 2>&1 |
+        Set-Variable -Name SevenZOut
+    $bbStatus = $LASTEXITCODE
+    Write-Progress -Activity "$activity" -Status "Done" -Completed
+    if ($bbStatus > 0) {
+        Write-Output $SevenZOut
+        exit 1
+    }
+}
+
+# On with the show
+
+# Make sure $Destination exists and do our work there.
+if ( -not (Test-Path $Destination -PathType 'Container') ) {
+    New-Item -ItemType 'Container' "$Destination" > $null
+}
+
+# CMake's file TO_NATIVE_PATH passive-aggressively omits the drive letter.
+Set-Location "$Destination"
+$Destination = $(Get-Item -Path ".\")
+Write-Output "Working in $Destination"
+
+# Check our last known state
+$destinationTag = "INVALID"
+$tagFile = "current_tag.txt"
+if ((Test-Path $tagFile -PathType 'Leaf') -and -not ($Force)) {
+    $destinationTag = Get-Content $tagFile
+}
+
+if ($destinationTag -ne $CurrentTag) {
+    Write-Output "Tag $CurrentTag not found. Refreshing."
+    Bootstrap7Zip
+    $activity = "Removing directories"
+    foreach ($oldItem in $CleanupItems) {
+        if (Test-Path $oldItem) {
+            Write-Progress -Activity "$activity" -Status "Removing $oldItem"
+            Remove-Item -force -recurse $oldItem
+        }
+    }
+    Write-Progress -Activity "$activity" -Status "Done" -Completed
+} else {
+    Write-Output "Tag $CurrentTag found. Skipping."
+    exit 0
+}
+
+# Download files
+foreach ($item in $Files) {
+    DownloadFile $item
+}
+
+# Download and extract archives
+foreach ($item in $Archives.GetEnumerator() | Sort-Object -property key) {
+    DownloadArchive $item.Name $item.Value
+}
+
+# Save our last known state
+Set-Content -Path $tagFile -Value "$CurrentTag"
